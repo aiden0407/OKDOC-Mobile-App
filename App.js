724df853cb3ae54e0c6ad01d495e3fe46c03bc6f
aiden0407 <cailyent0407@gmail.com>
2023-03-09 17:33:16 +0900

    [Feat] 기본 폰트 크기 조정

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-//Navigation
+//React
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//Font
+import { useFonts } from 'expo-font';
+import { Text, TextInput } from 'react-native';
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 //BottomTap
 import BottomTapNavigation from 'navigation/BottomTapNavigation';
@@ -8,20 +17,24 @@ import BottomTapNavigation from 'navigation/BottomTapNavigation';
 import TreatmentReservation from 'navigation/Home/TreatmentReservation';
 //Mypage
 import MyPageStackNavigation from 'navigation/MyPage';
-//Login/Register
+//Login
 import LoginStackNavigation from 'navigation/LoginStackNavigation';
 import NeedLoginNavigation from 'navigation/LoginStackNavigation/NeedLoginNavigation';
-
-//기본 폰트 크기 고정
-import { Text, TextInput } from 'react-native';
-Text.defaultProps = Text.defaultProps || {};
-Text.defaultProps.allowFontScaling = false;
-TextInput.defaultProps = TextInput.defaultProps || {};
-TextInput.defaultProps.allowFontScaling = false;
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    "Pretendard-Bold" : require('assets/fonts/Pretendard-Bold.otf'),
+    "Pretendard-Medium" : require('assets/fonts/Pretendard-Medium.otf'),
+    "Pretendard-Regular" : require('assets/fonts/Pretendard-Regular.otf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="BottomTapNavigation" >
