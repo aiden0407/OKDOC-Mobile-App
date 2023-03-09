93c5728efb0568ee493875a2674c5be84c5a41e8
aiden0407 <cailyent0407@gmail.com>
2023-03-09 17:43:12 +0900

    [Design] 디자인 시스템 상수 적용

--- src/constants/design/index.js
+++ src/constants/design/index.js
@@ -1,3 +1,64 @@
 export const COLOR = {
-    MAIN: '#5E27A4',
+    MAIN: '#5500CC',
+    SUB1: '#3E0095',
+    SUB2: '#E3D0FF',
+    SUB3: '#F1E8FF',
+    GRAY1: '#7A7A7A',
+    GRAY2: '#858585',
+    GRAY3: '#DBDBDB',
+    GRAY4: '#E8E8E8',
+    GRAY5: '#F8F8F8',
+}
+
+export const TYPOGRAPHY = {
+    T1: {
+        SIZE: '32px',
+        LEADING: '40px',
+    },
+    T2: {
+        SIZE: '24px',
+        LEADING: '32px',
+    },
+    T3: {
+        SIZE: '20px',
+        LEADING: '28px',
+    },
+    T4: {
+        SIZE: '18px',
+        LEADING: '26px',
+    },
+    T5: {
+        SIZE: '16px',
+        LEADING: '24px',
+    },
+    T6: {
+        SIZE: '14px',
+        LEADING: '20px',
+    },
+    T7: {
+        SIZE: '12px',
+        LEADING: '16px',
+    },
+    T8: {
+        SIZE: '11px',
+        LEADING: '16px',
+    },
+}
+
+export const BUTTON = {
+    FULL: {
+        WIDTH: '100%',
+        HEIGHT: '56px',
+    },
+    LARGE: {
+        WIDTH: '280px',
+        HEIGHT: '48px',
+    },
+    MEDIUM: {
+        WIDTH: '124px',
+        HEIGHT: '38px',
+    },
+    TINY: {
+        PADDING: '6px 20px',
+    },
 }
