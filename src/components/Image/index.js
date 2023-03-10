1f5528e5b0b115fc5defdc10cad21145a246de82
aiden0407 <cailyent0407@gmail.com>
2023-03-10 18:41:27 +0900

    [Add] 이미지 컴포넌트 추가

---
+++ src/components/Image/index.js
@@ -0,0 +1,8 @@
+//Styled Components
+import styled from 'styled-components/native';
+
+export const Image = styled.Image`
+  margin-top: ${(props) => `${props.marginTop ?? 0}px`};
+  width: ${(props) => `${props.width ?? 0}px`};
+  height: ${(props) => `${props.height ?? 0}px`};
+`;
