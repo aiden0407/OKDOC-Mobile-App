209a079845f6b07d2f305acc97b550e0467de00c
aiden0407 <cailyent0407@gmail.com>
2023-03-10 18:43:14 +0900

    [Design] 뒤로가기 오퍼시티 조정

--- src/components/NavigationBackArrow/index.js
+++ src/components/NavigationBackArrow/index.js
@@ -7,7 +7,7 @@ import { Ionicons } from '@expo/vector-icons';
 export default function NavigationBackArrow({ action }) {
   return (
     <TouchableOpacity
-      activeOpacity={0.4}
+      activeOpacity={0.6}
       onPress={action}
     >
       <Ionicons name="chevron-back" size={26} />
