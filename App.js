cadc6e98f3ac3f1efe315cc19eed1765a10f16e8
aiden0407 <cailyent0407@gmail.com>
2023-03-13 10:57:55 +0900

    [Chore] 폰트 로딩 중 스플래시 이미지 유지

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 //React
-import { useState } from 'react';
+import { useCallback } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //Default Settings
 import { useFonts } from 'expo-font';
-import { Text, TextInput } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { View, Text, TextInput } from 'react-native';
 import { COLOR } from 'constants/design'
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
@@ -23,39 +24,48 @@ import MyPageStackNavigation from 'navigation/MyPage';
 import LoginStackNavigation from 'navigation/LoginStackNavigation';
 import NeedLoginNavigation from 'navigation/LoginStackNavigation/NeedLoginNavigation';
 
+SplashScreen.preventAutoHideAsync();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  const [loaded] = useFonts({
-    "Pretendard-Bold" : require('assets/fonts/Pretendard-Bold.otf'),
-    "Pretendard-Medium" : require('assets/fonts/Pretendard-Medium.otf'),
-    "Pretendard-Regular" : require('assets/fonts/Pretendard-Regular.otf'),
+  const [fontsLoaded] = useFonts({
+    "Pretendard-Bold": require('assets/fonts/Pretendard-Bold.otf'),
+    "Pretendard-Medium": require('assets/fonts/Pretendard-Medium.otf'),
+    "Pretendard-Regular": require('assets/fonts/Pretendard-Regular.otf'),
   });
 
-  if (!loaded) {
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="BottomTapNavigation" >
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
 
-        <Stack.Group screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="BottomTapNavigation" component={BottomTapNavigation} />
-          {/* Home Components */}
-          <Stack.Screen name="TreatmentReservation" component={TreatmentReservation} />
-          {/* MyPage Components */}
-          <Stack.Screen name="MyPageStackNavigation" component={MyPageStackNavigation} />
-          {/* Etc. */}
-          <Stack.Screen name="NeedLoginNavigation" component={NeedLoginNavigation} />
-        </Stack.Group>
+          <Stack.Group screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="BottomTapNavigation" component={BottomTapNavigation} />
+            {/* Home Components */}
+            <Stack.Screen name="TreatmentReservation" component={TreatmentReservation} />
+            {/* MyPage Components */}
+            <Stack.Screen name="MyPageStackNavigation" component={MyPageStackNavigation} />
+            {/* Etc. */}
+            <Stack.Screen name="NeedLoginNavigation" component={NeedLoginNavigation} />
+          </Stack.Group>
 
-        <Stack.Group screenOptions={{ headerShown: false, presentation: 'transparentModal' }}>
-          <Stack.Screen name="LoginStackNavigation" component={LoginStackNavigation} />
-        </Stack.Group>
+          <Stack.Group screenOptions={{ headerShown: false, presentation: 'transparentModal' }}>
+            <Stack.Screen name="LoginStackNavigation" component={LoginStackNavigation} />
+          </Stack.Group>
 
-      </Stack.Navigator>
-    </NavigationContainer>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
