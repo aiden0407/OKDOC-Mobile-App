4742c55fde39dd4f3a01f2c47d884c6a645fcab4
aiden0407 <cailyent0407@gmail.com>
2023-03-13 16:13:48 +0900

    [Add] 증상/과목 상수화

---
+++ src/constants/service/index.js
@@ -0,0 +1,224 @@
+//symptoms icons
+import fluIcon from 'assets/icons/symptoms/flu.png';
+import headacheIcon from 'assets/icons/symptoms/headache.png';
+import highFeverIcon from 'assets/icons/symptoms/high-fever.png';
+import stomachacheIcon from 'assets/icons/symptoms/stomachache.png';
+import indigestionIcon from 'assets/icons/symptoms/indigestion.png';
+import acheIcon from 'assets/icons/symptoms/ache.png';
+import chillIcon from 'assets/icons/symptoms/chill.png';
+import constipationIcon from 'assets/icons/symptoms/constipation.png';
+import diarrheaIcon from 'assets/icons/symptoms/diarrhea.png';
+import heartburnIcon from 'assets/icons/symptoms/heartburn.png';
+import muscularPainIcon from 'assets/icons/symptoms/muscular-pain.png';
+import arthralgiaIcon from 'assets/icons/symptoms/arthralgia.png';
+
+//medical-subjects icons
+import internalMedicineIcon from 'assets/icons/medical-subjects/internal-medicine.png';
+import otolaryngologyIcon from 'assets/icons/medical-subjects/otolaryngology.png';
+import surgeryIcon from 'assets/icons/medical-subjects/surgery.png';
+import dentistryIcon from 'assets/icons/medical-subjects/dentistry.png';
+import pediatryIcon from 'assets/icons/medical-subjects/pediatry.png';
+import gynecologyIcon from 'assets/icons/medical-subjects/gynecology.png';
+import dermatologyIcon from 'assets/icons/medical-subjects/dermatology.png';
+import ophthalmologyIcon from 'assets/icons/medical-subjects/ophthalmology.png';
+import emergencyMedicineIcon from 'assets/icons/medical-subjects/emergency-medicine.png';
+import familyMedicineIcon from 'assets/icons/medical-subjects/family-medicine.png';
+import urologyIcon from 'assets/icons/medical-subjects/urology.png';
+import psychiatryIcon from 'assets/icons/medical-subjects/psychiatry.png';
+import orthopedicsIcon from 'assets/icons/medical-subjects/orthopedics.png';
+import rehabilitationMedicineIcon from 'assets/icons/medical-subjects/rehabilitation-medicine.png';
+import rheumatismIcon from 'assets/icons/medical-subjects/rheumatism.png';
+import cardiologyIcon from 'assets/icons/medical-subjects/cardiology.png';
+import laboratoryMedicineIcon from 'assets/icons/medical-subjects/laboratory-medicine.png';
+import infectionMedicineIcon from 'assets/icons/medical-subjects/infection-medicine.png';
+import endocrineMedicineIcon from 'assets/icons/medical-subjects/endocrine-medicine.png';
+import anesthesiologyIcon from 'assets/icons/medical-subjects/anesthesiology.png';
+import radiationOncologyIcon from 'assets/icons/medical-subjects/radiation-oncology.png';
+import thoracicSurgeryIcon from 'assets/icons/medical-subjects/thoracic-surgery.png';
+import pathologyIcon from 'assets/icons/medical-subjects/pathology.png';
+import nuclearMedicineIcon from 'assets/icons/medical-subjects/nuclear-medicine.png';
+import plasticSurgeryIcon from 'assets/icons/medical-subjects/plastic-surgery.png';
+import hematoOncologyIcon from 'assets/icons/medical-subjects/hemato-oncology.png';
+import pulmonologyIcon from 'assets/icons/medical-subjects/pulmonology.png';
+import neurologyIcon from 'assets/icons/medical-subjects/neurology.png';
+import neurosurgeryIcon from 'assets/icons/medical-subjects/neurosurgery.png';
+import nephrologyIcon from 'assets/icons/medical-subjects/nephrology.png';
+import radiologyIcon from 'assets/icons/medical-subjects/radiology.png';
+
+export const SYMPTOM = {
+    flu: {
+        NAME: '감기',
+        ICON: fluIcon
+    },
+    headache: {
+        NAME: '두통',
+        ICON: headacheIcon
+    },
+    highFever: {
+        NAME: '고열/미열',
+        ICON: highFeverIcon
+    },
+    stomachache: {
+        NAME: '복통',
+        ICON: stomachacheIcon
+    },
+    indigestion: {
+        NAME: '소화불량',
+        ICON: indigestionIcon
+    },
+    ache: {
+        NAME: '몸살',
+        ICON: acheIcon
+    },
+    chill: {
+        NAME: '오한',
+        ICON: chillIcon
+    },
+    constipation: {
+        NAME: '변비',
+        ICON: constipationIcon
+    },
+    diarrhea: {
+        NAME: '설사',
+        ICON: diarrheaIcon
+    },
+    heartburn: {
+        NAME: '속쓰림',
+        ICON: heartburnIcon
+    },
+    muscularPain: {
+        NAME: '근육통',
+        ICON: muscularPainIcon
+    },
+    arthralgia: {
+        NAME: '관절통',
+        ICON: arthralgiaIcon
+    },
+}
+
+export const SUBJECT = {
+    internalMedicine: {
+        NAME: '내과',
+        ICON: internalMedicineIcon
+    },
+    otolaryngology: {
+        NAME: '이비인후과',
+        ICON: otolaryngologyIcon
+    },
+    surgery: {
+        NAME: '외과',
+        ICON: surgeryIcon
+    },
+    dentistry: {
+        NAME: '치과',
+        ICON: dentistryIcon
+    },
+    pediatry: {
+        NAME: '소아청소년과',
+        ICON: pediatryIcon
+    },
+    gynecology: {
+        NAME: '산부인과',
+        ICON: gynecologyIcon
+    },
+    dermatology: {
+        NAME: '피부과',
+        ICON: dermatologyIcon
+    },
+    ophthalmology: {
+        NAME: '안과',
+        ICON: ophthalmologyIcon
+    },
+    emergencyMedicine: {
+        NAME: '응급의학과',
+        ICON: emergencyMedicineIcon
+    },
+    familyMedicine: {
+        NAME: '가정의학과',
+        ICON: familyMedicineIcon
+    },
+    urology: {
+        NAME: '비뇨기과',
+        ICON: urologyIcon
+    },
+    psychiatry: {
+        NAME: '정신건강의학과',
+        ICON: psychiatryIcon
+    },
+    orthopedics: {
+        NAME: '정형외과',
+        ICON: orthopedicsIcon
+    },
+    rehabilitationMedicine: {
+        NAME: '재활의학과',
+        ICON: rehabilitationMedicineIcon
+    },
+    rheumatism: {
+        NAME: '류마티스과',
+        ICON: rheumatismIcon
+    },
+    cardiology: {
+        NAME: '심장내과',
+        ICON: cardiologyIcon
+    },
+    laboratoryMedicine: {
+        NAME: '진단검사의학과',
+        ICON: laboratoryMedicineIcon
+    },
+    infectionMedicine: {
+        NAME: '감염내과',
+        ICON: infectionMedicineIcon
+    },
+    endocrineMedicine: {
+        NAME: '내분비과',
+        ICON: endocrineMedicineIcon
+    },
+    anesthesiology: {
+        NAME: '마취통증의학과',
+        ICON: anesthesiologyIcon
+    },
+    radiationOncology: {
+        NAME: '방사선종양학과',
+        ICON: radiationOncologyIcon
+    },
+    thoracicSurgery: {
+        NAME: '흉부외과',
+        ICON: thoracicSurgeryIcon
+    },
+    pathology: {
+        NAME: '병리과',
+        ICON: pathologyIcon
+    },
+    nuclearMedicine: {
+        NAME: '핵의학과',
+        ICON: nuclearMedicineIcon
+    },
+    plasticSurgery: {
+        NAME: '성형외과',
+        ICON: plasticSurgeryIcon
+    },
+    hematoOncology: {
+        NAME: '혈액종양내과',
+        ICON: hematoOncologyIcon
+    },
+    pulmonology: {
+        NAME: '호흡기내과',
+        ICON: pulmonologyIcon
+    },
+    neurology: {
+        NAME: '신경과',
+        ICON: neurologyIcon
+    },
+    neurosurgery: {
+        NAME: '신경외과',
+        ICON: neurosurgeryIcon
+    },
+    nephrology: {
+        NAME: '신장내과',
+        ICON: nephrologyIcon
+    },
+    radiology: {
+        NAME: '영상의학과',
+        ICON: radiologyIcon
+    },
+}
