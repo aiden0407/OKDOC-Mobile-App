9aa68288b07ba9613c64caedcefbf65ee452a86d
aiden0407 <cailyent0407@gmail.com>
2023-03-14 15:25:08 +0900

    [Design] 이미지에 circle 속성 추가

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -5,4 +5,5 @@ export const Image = styled.Image`
   margin-top: ${(props) => `${props.marginTop ?? 0}px`};
   width: ${(props) => `${props.width ?? 0}px`};
   height: ${(props) => `${props.height ?? 0}px`};
+  ${(props) => props.circle && `border-radius: 100px`}
 `;
