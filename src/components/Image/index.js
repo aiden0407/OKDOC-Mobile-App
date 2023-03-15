2c4a7dd91661d111c306d0daf2260ce667caf9cd
aiden0407 <cailyent0407@gmail.com>
2023-03-15 17:53:29 +0900

    [Refactor] 불필요한 props 제거

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -2,7 +2,6 @@
 import styled from 'styled-components/native';
 
 export const Image = styled.Image`
-  margin-top: ${(props) => `${props.marginTop ?? 0}px`};
   width: ${(props) => `${props.width ?? 0}px`};
   height: ${(props) => `${props.height ?? 0}px`};
   ${(props) => props.circle && `border-radius: 100px`}
