173600bdf65e4773f178e09a38e777bacb148f0e
aiden0407 <cailyent0407@gmail.com>
2023-03-15 16:48:10 +0900

    [Feat] AppProvider 적용

--- App.js
+++ App.js
@@ -2,25 +2,25 @@
 import { useCallback } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppProvider } from "context/AppContext";
 
 //Default Settings
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { COLOR } from 'constants/design'
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 TextInput.defaultProps = TextInput.defaultProps || {};
 TextInput.defaultProps.allowFontScaling = false;
 TextInput.defaultProps.placeholderTextColor = COLOR.GRAY2;
+TouchableOpacity.defaultProps = TouchableOpacity.defaultProps || {};
+TouchableOpacity.defaultProps.activeOpacity = 0.6;
 
-//BottomTap
+//navigation
 import BottomTapNavigation from 'navigation/BottomTapNavigation';
-//Home
-import TreatmentReservation from 'navigation/Home/TreatmentReservation';
-//Mypage
+import TelemedicineReservation from 'navigation/Home/TelemedicineReservation';
 import MyPageStackNavigation from 'navigation/MyPage';
-//Login
 import LoginStackNavigation from 'navigation/LoginStackNavigation';
 import NeedLoginNavigation from 'navigation/LoginStackNavigation/NeedLoginNavigation';
 
@@ -46,26 +46,25 @@ export default function App() {
   }
 
   return (
-    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+    <AppProvider>
       <NavigationContainer>
-        <Stack.Navigator>
+        <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+          <Stack.Navigator>
 
-          <Stack.Group screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="BottomTapNavigation" component={BottomTapNavigation} />
-            {/* Home Components */}
-            <Stack.Screen name="TreatmentReservation" component={TreatmentReservation} />
-            {/* MyPage Components */}
-            <Stack.Screen name="MyPageStackNavigation" component={MyPageStackNavigation} />
-            {/* Etc. */}
-            <Stack.Screen name="NeedLoginNavigation" component={NeedLoginNavigation} />
-          </Stack.Group>
+            <Stack.Group screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="BottomTapNavigation" component={BottomTapNavigation} />
+              <Stack.Screen name="TelemedicineReservation" component={TelemedicineReservation} />
+              <Stack.Screen name="MyPageStackNavigation" component={MyPageStackNavigation} />
+              <Stack.Screen name="NeedLoginNavigation" component={NeedLoginNavigation} />
+            </Stack.Group>
 
-          <Stack.Group screenOptions={{ headerShown: false, presentation: 'transparentModal' }}>
-            <Stack.Screen name="LoginStackNavigation" component={LoginStackNavigation} />
-          </Stack.Group>
+            <Stack.Group screenOptions={{ headerShown: false, presentation: 'transparentModal' }}>
+              <Stack.Screen name="LoginStackNavigation" component={LoginStackNavigation} />
+            </Stack.Group>
 
-        </Stack.Navigator>
+          </Stack.Navigator>
+        </View>
       </NavigationContainer>
-    </View>
+    </AppProvider>
   );
 }
