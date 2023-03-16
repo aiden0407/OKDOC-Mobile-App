438f48c188a688198bc29cbf72ebb344d7c64234
aiden0407 <cailyent0407@gmail.com>
2023-03-16 15:59:27 +0900

    [Design] 로그인 필요 컴포넌트 사용

--- src/screens/Login/NeedLogin/index.js
+++ src/screens/Login/NeedLogin/index.js
@@ -2,14 +2,8 @@
 import { useEffect } from 'react';
 
 //Components
-import { COLOR } from 'constants/design'
 import { SafeArea, ContainerCenter } from 'components/Layout';
-import { Text } from 'components/Text';
-import { Image } from 'components/Image';
-import { SolidButton } from 'components/Button';
-
-//Assets
-import profileCard from 'assets/images/profile_card.png';
+import NeedLogin from 'components/NeedLogin';
 
 export default function NeedLoginScreen({ navigation, route }) {
 
@@ -22,17 +16,10 @@ export default function NeedLoginScreen({ navigation, route }) {
   return (
     <SafeArea>
       <ContainerCenter>
-
-        <Image source={profileCard} marginTop={-80} width={94} height={55}/>
-        <Text T3 bold marginTop={24}>로그인이 필요해요</Text>
-        <Text T6 medium center color={COLOR.GRAY1} marginTop={12}>해외에서도 비대면으로{'\n'}한국 대학병원 전문의를 만나보세요</Text>
-        <SolidButton
-          large
-          marginTop={24}
-          text="로그인 / 회원가입"
-          action={() => navigation.navigate('LoginStackNavigation')}
+        <NeedLogin 
+          marginTop={-80}
+          action={() => navigation.navigate('LoginStackNavigation')} 
         />
-        
       </ContainerCenter>
     </SafeArea>
   );
