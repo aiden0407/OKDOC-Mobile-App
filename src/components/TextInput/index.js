5d5f86dea153b93fa8975fa2be2ef93e37cdfaeb
aiden0407 <cailyent0407@gmail.com>
2023-03-22 16:58:34 +0900

    [Update] 박스인풋 크기 옵션 추가

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,6 +1,16 @@
 //Styled Components
 import styled from 'styled-components/native';
-import { COLOR, TYPOGRAPHY } from 'constants/design'
+import { COLOR, TYPOGRAPHY, INPUT_BOX } from 'constants/design'
+
+export const LineInput = styled.TextInput`
+  width: 100%;
+  padding-bottom: 8px;
+  border-bottom-width: 1.5px;
+  border-color: ${COLOR.GRAY3};
+  font-family: 'Pretendard-Regular';
+  font-size: ${TYPOGRAPHY.T5.SIZE};
+  color: ${(props) => props.editable===false ? COLOR.GRAY0 : '#000000' };
+`;
 
 export const BorderInput = styled.TextInput`
   width: 100%;
@@ -13,38 +23,41 @@ export const BorderInput = styled.TextInput`
   font-size: ${TYPOGRAPHY.T5.SIZE};
 `;
 
-export const LineInput = styled.TextInput`
-  width: 100%;
-  padding-bottom: 8px;
-  border-bottom-width: 1.5px;
-  border-color: ${COLOR.GRAY3};
-  font-family: 'Pretendard-Regular';
-  font-size: ${TYPOGRAPHY.T5.SIZE};
-  color: ${(props) => props.editable===false ? COLOR.GRAY0 : '#000000' };
-`;
+function inputBoxBackgroundOptionSelector(large, medium){
+  if(large) return `padding: ${INPUT_BOX.LARGE.BACKGROUND_PADDING};`
+  if(medium) return `padding: ${INPUT_BOX.MEDIUM.BACKGROUND_PADDING};`
+  return `padding: ${INPUT_BOX.DEFAULT.BACKGROUND_PADDING};`
+}
 
-export function BoxInputMedium({ ...props }) {
+function inputBoxTextInputOptionSelector(large, medium){
+  if(large) return `padding: ${INPUT_BOX.LARGE.TEXT_INPUT_PADDING}; height: ${INPUT_BOX.LARGE.TEXT_INPUT_HEIGHT};`
+  if(medium) return `padding: ${INPUT_BOX.MEDIUM.TEXT_INPUT_PADDING}; height: ${INPUT_BOX.MEDIUM.TEXT_INPUT_HEIGHT};`
+  return `height: ${INPUT_BOX.DEFAULT.TEXT_INPUT_HEIGHT};`
+}
+
+export function BoxInput({ ...props }) {
   return (
-    <MediumTextInputBackground
-      editable={props.editable}
+    <TextInputBackground
       marginTop={props.marginTop}
+      editable={props.editable}
+      large={props.large}
+      medium={props.medium}
     >
-      <MediumTextInput {...props} marginTop={0}/>
-    </MediumTextInputBackground>
+      <TextInput multiline={props.medium || props.large} {...props} marginTop={0} />
+    </TextInputBackground>
   );
 }
 
-export const MediumTextInputBackground = styled.View`
+export const TextInputBackground = styled.View`
+  ${(props) => inputBoxBackgroundOptionSelector(props.large, props.medium)}
   width: 100%;
-  padding: 12px 6px 16px 16px;
   background-color: ${(props) => props.editable===false ? COLOR.GRAY4 : COLOR.GRAY6 };
   border-radius: 5px;
 `;
 
-export const MediumTextInput = styled.TextInput`
+export const TextInput = styled.TextInput`
+  ${(props) => inputBoxTextInputOptionSelector(props.large, props.medium)}
   width: 100%;
-  height: 66px;
-  padding: 0 6px 0 0;
   font-family: 'Pretendard-Regular';
   font-size: ${TYPOGRAPHY.T6.SIZE};
   color: ${(props) => props.editable===false ? '#333333' : '#000000' };
