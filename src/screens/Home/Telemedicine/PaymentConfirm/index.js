d5f22b5fd5f7d57bf18e3736c050c3006d8d0aa5
aiden0407 <cailyent0407@gmail.com>
2023-03-22 17:07:50 +0900

    [Add] 결제 완료 스크린 추가

---
+++ src/screens/Home/Telemedicine/PaymentConfirm/index.js
@@ -0,0 +1,89 @@
+//React
+import { useState, useContext } from 'react';
+import { AppContext } from 'context/AppContext';
+import styled from 'styled-components/native';
+
+//Components
+import { COLOR } from 'constants/design';
+import { SYMPTOM, SUBJECT } from 'constants/service';
+import { Ionicons } from '@expo/vector-icons';
+import { SafeArea, Container, Row, DividingLine } from 'components/Layout';
+import { Text } from 'components/Text';
+import { SolidButton } from 'components/Button';
+
+export default function PaymentConfirmScreen({ navigation }) {
+
+  const { state: { telemedicineReservationStatus }, dispatch } = useContext(AppContext);
+
+  function handleConfirm() {
+    dispatch({ type: 'TELEMEDICINE_RESERVATION_CONFIRMED' });
+    navigation.navigate('BottomTapNavigation');
+  }
+
+  return (
+    <SafeArea>
+      <Container>
+        <DevidedContainer>
+          <Text T3 bold marginTop={30}>결제가 완료되었어요</Text>
+          <Text T3 bold color={COLOR.MAIN} marginTop={9}>비용 120,000원</Text>
+          <Row marginTop={18}>
+            <Text T6 medium color={COLOR.GRAY1} marginRight={42}>결제 금액</Text>
+            <Text T6 color={COLOR.GRAY1}>120,000원 | 일시불</Text>
+          </Row>
+          <Row marginTop={6}>
+            <Text T6 medium color={COLOR.GRAY1} marginRight={42}>결제 수단</Text>
+            <Text T6 color={COLOR.GRAY1}>삼성 마스타 5188</Text>
+          </Row>
+          <Row marginTop={6}>
+            <Text T6 medium color={COLOR.GRAY1} marginRight={42}>결제 일시</Text>
+            <Text T6 color={COLOR.GRAY1}>23.04.07 (11:30)</Text>
+          </Row>
+        </DevidedContainer>
+
+        <DividingLine marginTop={30} />
+
+        <DevidedContainer>
+          <Text T3 bold marginTop={30}>예약하신 내역을 확인해주세요</Text>
+          <Row align marginTop={15}>
+            <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6} />
+            <Text T6 medium>{telemedicineReservationStatus.doctorInfo.name} 의사 (을지대학교병원)</Text>
+          </Row>
+          <Row align marginTop={12}>
+            <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6} />
+            {
+              telemedicineReservationStatus.category === 'symptom'
+              && <Text T6 medium>{SYMPTOM[telemedicineReservationStatus.item].NAME} / 진료</Text>
+            }
+            {
+              telemedicineReservationStatus.category === 'medicalSubject'
+              && <Text T6 medium>{SUBJECT[telemedicineReservationStatus.item].NAME} / 진료</Text>
+            }
+          </Row>
+          <Row align marginTop={12}>
+            <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6} />
+            <Text T6 medium>2023년 4월 {telemedicineReservationStatus.date} ({telemedicineReservationStatus.time})</Text>
+          </Row>
+          <Row align marginTop={12}>
+            <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6} />
+            <Text T6 medium>예약자: {telemedicineReservationStatus.profileInfo.name}</Text>
+          </Row>
+        </DevidedContainer>
+
+        <Container />
+
+        <DevidedContainer>
+          <SolidButton
+            text="확인"
+            marginBottom={20}
+            action={() => handleConfirm()}
+          />
+        </DevidedContainer>
+      </Container>
+    </SafeArea>
+  );
+}
+
+const DevidedContainer = styled.View`
+  width: 100%;
+  padding: 0 20px;
+`;
