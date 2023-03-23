e3f2c94262ad0201880951b775d73731c2a6bfc4
aiden0407 <cailyent0407@gmail.com>
2023-03-23 16:05:33 +0900

    [Add] 마이페이지 메인 스택, 외부 스택 분리

---
+++ src/navigation/MyPage/MyPageStackNavigation/index.js
@@ -0,0 +1,72 @@
+//Navigation
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MyPageProfileDetailScreen from 'screens/MyPage/Profile/MyPageDetail';
+import InquiryScreen from 'screens/MyPage/Inquiry';
+import PolicyScreen from 'screens/MyPage/Policy';
+import PolicyDetailScreen from 'screens/MyPage/Policy/Detail';
+import FaqScreen from 'screens/MyPage/FAQ';
+import NotificationScreen from 'screens/MyPage/Notification';
+import NotificationDetailScreen from 'screens/MyPage/Notification/Detail';
+
+//Components
+import NavigationBackArrow from 'components/NavigationBackArrow';
+
+const Stack = createNativeStackNavigator();
+
+export default function MyPageInnerStackNavigation({ navigation }) {
+
+  return (
+    <Stack.Navigator>
+      <Stack.Group
+        screenOptions={{
+          headerLargeTitleShadowVisible: false,
+          headerLeft: () => <NavigationBackArrow action={() => navigation.goBack()} />,
+        }}
+      >
+        <Stack.Screen
+          name="MyPageProfileDetail"
+          component={MyPageProfileDetailScreen}
+          options={{title: '프로필 정보'}}
+        />
+        <Stack.Screen
+          name="Inquiry"
+          component={InquiryScreen}
+          options={{ title: '1:1 문의' }}
+        />
+        <Stack.Screen
+          name="Policy"
+          component={PolicyScreen}
+          options={{ title: '서비스 약관 & 정책' }}
+        />
+        <Stack.Screen
+          name="FAQ"
+          component={FaqScreen}
+          options={{ title: '자주하는 질문' }}
+        />
+        <Stack.Screen
+          name="Notification"
+          component={NotificationScreen}
+          options={{ title: '공지사항' }}
+        />
+      </Stack.Group>
+
+      <Stack.Group screenOptions={{ headerLargeTitleShadowVisible: false }}>
+        <Stack.Screen
+          name="PolicyDetail"
+          component={PolicyDetailScreen}
+          options={{
+            headerLeft: () => <NavigationBackArrow action={() => navigation.navigate('Policy')} />,
+          }}
+        />
+        <Stack.Screen
+          name="NotificationDetail"
+          component={NotificationDetailScreen}
+          options={{
+            title: '공지사항',
+            headerLeft: () => <NavigationBackArrow action={() => navigation.navigate('Notification')} />,
+          }}
+        />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+}
--- src/navigation/MyPage/index.js
+++ src/navigation/MyPage/index.js
@@ -1,75 +1,32 @@
 //Navigation
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AccountSettingScreen from 'screens/MyPage/AccountSetting' ;
-import MyPageProfileDetailScreen from 'screens/MyPage/Profile/MyPageDetail';
-import InquiryScreen from 'screens/MyPage/Inquiry';
-import PolicyScreen from 'screens/MyPage/Policy';
-import PolicyDetailScreen from 'screens/MyPage/Policy/Detail';
-import FaqScreen from 'screens/MyPage/FAQ';
-import NotificationScreen from 'screens/MyPage/Notification';
-import NotificationDetailScreen from 'screens/MyPage/Notification/Detail';
+import MyPageScreen from 'screens/MyPage';
+import AccountSettingScreen from 'screens/MyPage/AccountSetting';
 
 //Components
 import NavigationBackArrow from 'components/NavigationBackArrow';
 
 const Stack = createNativeStackNavigator();
 
-export default function MypageStackNavigation({ navigation }) {
+export default function MyPageInnerStackNavigation({ navigation }) {
 
   return (
     <Stack.Navigator>
-      <Stack.Group
-        screenOptions={{
-          headerLargeTitleShadowVisible: false,
-          headerLeft: () => <NavigationBackArrow action={() => navigation.goBack()} />,
+      <Stack.Group screenOptions={{ headerLargeTitleShadowVisible: false }}>
+        <Stack.Screen
+          name="MyPageMain"
+          component={MyPageScreen}
+          options={{
+            title: '마이페이지',
+            headerLeft: () => <NavigationBackArrow action={() => navigation.goBack()} />,
         }}
-      >
+        />
         <Stack.Screen
           name="AccountSetting"
           component={AccountSettingScreen}
-          options={{title: '계정 설정'}}
-        />
-        <Stack.Screen
-          name="MyPageProfileDetail"
-          component={MyPageProfileDetailScreen}
-          options={{title: '프로필 정보'}}
-        />
-        <Stack.Screen
-          name="Inquiry"
-          component={InquiryScreen}
-          options={{ title: '1:1 문의' }}
-        />
-        <Stack.Screen
-          name="Policy"
-          component={PolicyScreen}
-          options={{ title: '서비스 약관 & 정책' }}
-        />
-        <Stack.Screen
-          name="FAQ"
-          component={FaqScreen}
-          options={{ title: '자주하는 질문' }}
-        />
-        <Stack.Screen
-          name="Notification"
-          component={NotificationScreen}
-          options={{ title: '공지사항' }}
-        />
-      </Stack.Group>
-
-      <Stack.Group screenOptions={{ headerLargeTitleShadowVisible: false }}>
-        <Stack.Screen
-          name="PolicyDetail"
-          component={PolicyDetailScreen}
           options={{
-            headerLeft: () => <NavigationBackArrow action={() => navigation.navigate('Policy')} />,
-          }}
-        />
-        <Stack.Screen
-          name="NotificationDetail"
-          component={NotificationDetailScreen}
-          options={{
-            title: '공지사항',
-            headerLeft: () => <NavigationBackArrow action={() => navigation.navigate('Notification')} />,
+            title: '계정 설정',
+            headerLeft: () => <NavigationBackArrow action={() => navigation.navigate('MyPageMain')} />,
           }}
         />
       </Stack.Group>
