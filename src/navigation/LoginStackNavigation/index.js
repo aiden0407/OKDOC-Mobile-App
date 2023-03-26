735c9918dbd14ad00fe28a4ad4a62e52a376fbb8
aiden0407 <cailyent0407@gmail.com>
2023-03-26 19:13:21 +0900

    [Update] 이메일/비밀번호 찾기 navigation 추가

--- src/navigation/LoginStackNavigation/index.js
+++ src/navigation/LoginStackNavigation/index.js
@@ -1,6 +1,7 @@
 //Navigation
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from 'screens/Login';
+import FindEmailPasswordScreen from 'screens/Login/FindEmailPassword';
 
 //Components
 import NavigationBackArrow from 'components/NavigationBackArrow';
@@ -10,16 +11,25 @@ const Stack = createNativeStackNavigator();
 export default function LoginStackNavigation({ navigation }) {
   return (
     <Stack.Navigator initialRouteName="Login" >
+      <Stack.Group screenOptions={{ headerLargeTitleShadowVisible: false }}>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: '로그인',
+            headerLeft: () => <NavigationBackArrow action={()=>navigation.goBack()} />,
+          }}
+        />
+        <Stack.Screen
+          name="FindEmailPassword"
+          component={FindEmailPasswordScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Group>
 
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{
-          title: '로그인',
-          headerLargeTitleShadowVisible: false,
-          headerLeft: () => <NavigationBackArrow action={()=>navigation.goBack()} />,
-        }}
-      />
+      
 
     </Stack.Navigator>
   );
