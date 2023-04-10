408054b62539e84b95e455acec5ac007de836a4b
aiden0407 <cailyent0407@gmail.com>
2023-04-11 08:54:52 +0900

    [Update] 전화번호 dispatch 추가

--- src/screens/Login/Register/PersonalInformation/index.js
+++ src/screens/Login/Register/PersonalInformation/index.js
@@ -1,64 +1,77 @@
 //React
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { AppContext } from 'context/AppContext';
 import styled from 'styled-components/native';
 
 //Components
 import { COLOR, TYPOGRAPHY, INPUT_BOX } from 'constants/design';
-import { Dimensions, Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { SafeArea, Container, Row } from 'components/Layout';
+import { Dimensions } from 'react-native';
+import { SafeArea, KeyboardAvoiding, Container, Row } from 'components/Layout';
 import { Text } from 'components/Text';
 import { SolidButton } from 'components/Button';
 
 export default function RegisterPolicyScreen({ navigation }) {
 
-  const [allPolicyAgreement, setAllPolicyAgreement] = useState('');
+  const { state: { registerStatus }, dispatch } = useContext(AppContext);
+  const [countryCode, setCountryCode] = useState('+82');
+  const [phoneNumber, setPhoneNumber] = useState(registerStatus?.phoneNumber);
   const windowWidth = Dimensions.get('window').width;
 
-
   function handleNextScreen() {
+    dispatch({
+      type: 'REGISTER_PASSPORT_PHONE_NUMBER',
+      countryCode: countryCode,
+      phoneNumber: phoneNumber,
+    });
     navigation.navigate('EmailPassword');
   }
 
   return (
     <SafeArea>
-      <Container paddingHorizontal={20}>
-        <Container>
+      <KeyboardAvoiding>
+        <Container paddingHorizontal={20}>
+          <Container>
 
-          <Text T3 bold marginTop={30}>인증된 아래 정보들을 확인해주세요</Text>
-          <Text T6 color={COLOR.GRAY1} marginTop={12}>국가번호와 전화번호를 정확히 입력해주세요</Text>
+            <Text T3 bold marginTop={30}>인증된 아래 정보들을 확인해주세요</Text>
+            <Text T6 color={COLOR.GRAY1} marginTop={12}>국가번호와 전화번호를 정확히 입력해주세요</Text>
 
-          <Text T6 bold marginTop={30}>한글 성명</Text>
-          <TextBox>
-            <Text T5>이준범</Text>
-          </TextBox>
-          <Text T6 bold marginTop={30}>생년월일</Text>
-          <TextBox>
-            <Text T5>1998.04.07</Text>
-          </TextBox>
-          <Text T6 bold marginTop={30}>성별</Text>
-          <Row marginTop={12} gap={12}>
-            <SolidButton medium text="남성" disabled={false} />
-            <SolidButton medium text="여성" disabled={true} />
-          </Row>
-          <Text T6 bold marginTop={30}>전화번호</Text>
-          <Row marginTop={12} gap={6}>
-            <CountryCallingCodeBox>
-              <Text T5>{`+82`}</Text>
-            </CountryCallingCodeBox>
-            <PhoneNumberBox windowWidth={windowWidth} />
-          </Row>
+            <Text T6 bold marginTop={30}>한글 성명</Text>
+            <TextBox>
+              <Text T5>{registerStatus?.name ?? ' '}</Text>
+            </TextBox>
+            <Text T6 bold marginTop={30}>생년월일</Text>
+            <TextBox>
+              <Text T5>{registerStatus?.birth?.toISOString()?.split('T')[0] ?? ' '}</Text>
+            </TextBox>
+            {/* <Text T6 bold marginTop={30}>성별</Text>
+            <Row marginTop={12} gap={12}>
+              <SolidButton medium text="남성" disabled={false} />
+              <SolidButton medium text="여성" disabled={true} />
+            </Row> */}
+            <Text T6 bold marginTop={30}>전화번호</Text>
+            <Row marginTop={12} gap={6}>
+              <CountryCallingCodeBox>
+                <Text T5>{countryCode}</Text>
+              </CountryCallingCodeBox>
+              <PhoneNumberBox
+                placeholder="전화번호"
+                windowWidth={windowWidth}
+                value={phoneNumber}
+                onChangeText={setPhoneNumber}
+                inputMode="numeric"
+              />
+            </Row>
 
+          </Container>
+
+          <SolidButton
+            text="다음"
+            marginBottom={20}
+            disabled={!phoneNumber}
+            action={() => handleNextScreen()}
+          />
         </Container>
-
-        <SolidButton
-          text="다음"
-          marginBottom={20}
-          disabled={false}
-          action={() => handleNextScreen()}
-        />
-      </Container>
+      </KeyboardAvoiding>
     </SafeArea>
   );
 }
