474719aa7f945e294ad19bb5dc8ac7d042bd571d
aiden0407 <cailyent0407@gmail.com>
2023-04-12 10:59:02 +0900

    [Refactor] mock 데이터 ApiContext로 이동

--- src/screens/Home/Telemedicine/Reservation/index.js
+++ src/screens/Home/Telemedicine/Reservation/index.js
@@ -1,142 +1,22 @@
 //React
 import { useState, useContext } from 'react';
 import { AppContext } from 'context/AppContext';
+import { ApiContext } from 'context/ApiContext';
 import styled from 'styled-components/native';
 
 //Components
 import { COLOR } from 'constants/design';
 import { Ionicons } from '@expo/vector-icons';
-import { SafeArea, Container, ScrollView, DividingLine } from 'components/Layout';
+import { SafeArea, Container, ScrollView, Row, DividingLine } from 'components/Layout';
 import { Text } from 'components/Text';
 import { Image } from 'components/Image';
 
 export default function ReservationScreen({ navigation, route }) {
 
-  const { state: {telemedicineReservationStatus}, dispatch } = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
+  const { state: { bookableData } } = useContext(ApiContext);
   const [dateIndex, setDateIndex] = useState(0);
   const [timeIndex, setTimeIndex] = useState(0);
-
-  const bookableData = [
-    ['27일', '목요일', [
-      ['11:00',[
-        {
-          name: '이준범',
-          image: 'https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*',
-          hospital: '을지대병원',
-          subject: '외과',
-          medicalField: ['외과 수술', '13년차', '전문의'],
-        },
-        {
-          name: '김형도',
-          image: 'https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg',
-          hospital: '을지대병원',
-          subject: '내과',
-          medicalField: ['내과 수술', '20년차', '베테랑'],
-        },
-      ]],
-      ['11:30',[
-        {
-          name: '이준범',
-          image: 'https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*',
-          hospital: '을지대병원',
-          subject: '외과',
-          medicalField: ['외과 수술', '13년차', '전문의'],
-        },
-        {
-          name: '장윤희',
-          image: 'https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=',
-          hospital: '을지대병원',
-          subject: '산부인과',
-          medicalField: ['5년차', '전문의'],
-        },
-        {
-          name: '김형도',
-          image: 'https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg',
-          hospital: '을지대병원',
-          subject: '내과',
-          medicalField: ['내과 수술', '20년차', '베테랑'],
-        },
-        {
-          name: '해밍턴',
-          image: 'https://cdn.houstonpublicmedia.org/wp-content/uploads/2015/09/22105535/Nuila.Ricardo.E.143882.Internal.Med_.jpg',
-          hospital: '을지대병원',
-          subject: '신경외과',
-          medicalField: ['정신과 상담', '신경 수술', , '17년차', '베테랑'],
-        },
-      ]]
-    ]],
-    ['28일', '금요일', [
-      ['12:30',[
-        {
-          name: '해밍턴',
-          image: 'https://cdn.houstonpublicmedia.org/wp-content/uploads/2015/09/22105535/Nuila.Ricardo.E.143882.Internal.Med_.jpg',
-          hospital: '을지대병원',
-          subject: '신경외과',
-          medicalField: ['정신과 상담', '신경 수술', , '17년차', '베테랑'],
-        },
-        {
-          name: '이준범',
-          image: 'https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*',
-          hospital: '을지대병원',
-          subject: '외과',
-          medicalField: ['외과 수술', '13년차', '전문의'],
-        },
-        {
-          name: '김형도',
-          image: 'https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg',
-          hospital: '을지대병원',
-          subject: '내과',
-          medicalField: ['내과 수술', '20년차', '베테랑'],
-        },
-      ]],
-      ['16:00',[
-        {
-          name: '이준범',
-          image: 'https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*',
-          hospital: '을지대병원',
-          subject: '외과',
-          medicalField: ['외과 수술', '13년차', '전문의'],
-        },
-        {
-          name: '장윤희',
-          image: 'https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=',
-          hospital: '을지대병원',
-          subject: '산부인과',
-          medicalField: ['5년차', '전문의'],
-        },
-        {
-          name: '김형도',
-          image: 'https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg',
-          hospital: '을지대병원',
-          subject: '내과',
-          medicalField: ['내과 수술', '20년차', '베테랑'],
-        },
-        {
-          name: '해밍턴',
-          image: 'https://cdn.houstonpublicmedia.org/wp-content/uploads/2015/09/22105535/Nuila.Ricardo.E.143882.Internal.Med_.jpg',
-          hospital: '을지대병원',
-          subject: '신경외과',
-          medicalField: ['정신과 상담', '신경 수술', , '17년차', '베테랑'],
-        },
-      ]],
-      ['18:30',[
-        {
-          name: '해밍턴',
-          image: 'https://cdn.houstonpublicmedia.org/wp-content/uploads/2015/09/22105535/Nuila.Ricardo.E.143882.Internal.Med_.jpg',
-          hospital: '을지대병원',
-          subject: '신경외과',
-          medicalField: ['정신과 상담', '신경 수술', , '17년차', '베테랑'],
-        },
-        {
-          name: '장윤희',
-          image: 'https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=',
-          hospital: '을지대병원',
-          subject: '산부인과',
-          medicalField: ['5년차', '전문의'],
-        },
-      ]]
-    ]],
-  ];
 
   function handleSelectDoctor(date, time, doctorInfo) {
     dispatch({ 
@@ -155,7 +35,6 @@ export default function ReservationScreen({ navigation, route }) {
 
           <ReservationContainer>
             <Text T3 bold marginTop={30}>진료시간을 선택해주세요</Text>
-
             <Text T6 medium marginTop={36}>날짜선택</Text>
             <DateContainer>
               <ScrollView horizontal showsHorizontalScrollIndicator={false}>
@@ -210,11 +89,11 @@ export default function ReservationScreen({ navigation, route }) {
                 <DoctorColumn>
                   <Text T4 bold>{item.name} 의사</Text>
                   <Text T7 medium color={COLOR.GRAY1}>{item.hospital} / {item.subject}</Text>
-                  <MedicalFieldRow>
+                  <Row marginTop={12}>
                     { item.medicalField.map((item, index) => 
-                      <Text key={`field${index}`} T7 color={COLOR.GRAY2}>#{item} </Text>
+                      <Text key={`field${index}`} T7 color={COLOR.GRAY1}>#{item} </Text>
                     )}
-                  </MedicalFieldRow>
+                  </Row>
                 </DoctorColumn>
                 <Ionicons name="chevron-forward" size={24} color={COLOR.MAIN}/>
               </DoctorRow>
@@ -279,9 +158,4 @@ const DoctorRow = styled.Pressable`
 const DoctorColumn = styled.View`
   margin-left: 24px;
   flex: 1;
-`;
-
-const MedicalFieldRow = styled.View`
-  margin-top: 12px;
-  flex-direction: row;
 `;
