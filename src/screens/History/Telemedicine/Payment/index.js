12777412395db6af90efc985eba302606c3cd9b9
aiden0407 <cailyent0407@gmail.com>
2023-04-18 17:32:25 +0900

    [Temp] 결제 페이지 추가

---
+++ src/screens/History/Telemedicine/Payment/index.js
@@ -0,0 +1,45 @@
+//React
+import { useState, useEffect, useRef, useContext } from 'react';
+import { AppContext } from 'context/AppContext';
+import styled from 'styled-components/native';
+
+//Components
+import { SafeArea, ContainerCenter } from 'components/Layout';
+import { Text } from 'components/Text';
+
+export default function PaymentScreen({ navigation, route }) {
+
+  const telemedicineData = route.params.telemedicineData;
+  const [count, setCount] = useState(3);
+  const savedCallback = useRef();
+
+  const callback = () => {
+    if(count === 1){
+      navigation.replace('PaymentComplete', {
+        telemedicineData: telemedicineData,
+      });
+    }else{
+      setCount(count-1);
+    }
+  }
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    const timer = setInterval(tick, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <SafeArea>
+      <ContainerCenter>
+        <Text T5 bold>PG사 결제창 스크린: {count}초</Text>
+      </ContainerCenter>
+    </SafeArea>
+  );
+}
