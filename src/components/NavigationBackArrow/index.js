9d37fb7ca3cfddb17d5935645963896f009a51a9
aiden0407 <cailyent0407@gmail.com>
2023-04-19 11:02:41 +0900

    [Update] android기기 navigation arrow 위치 조정

--- src/components/NavigationBackArrow/index.js
+++ src/components/NavigationBackArrow/index.js
@@ -1,13 +1,15 @@
 //Core Components
+import * as Device from 'expo-device';
 import { TouchableOpacity } from 'react-native';
 
 //Components
 import { Ionicons } from '@expo/vector-icons';
 
 export default function NavigationBackArrow({ action }) {
+
   return (
     <TouchableOpacity onPress={action}>
-      <Ionicons name="chevron-back" size={26} />
+      <Ionicons name="chevron-back" size={26} marginTop={Device.osName==='Android' && 5}/>
     </TouchableOpacity>
   );
 }
