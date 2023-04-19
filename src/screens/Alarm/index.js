2ff6fe8d872dc5ba90c41207d1be81d8799840e9
aiden0407 <cailyent0407@gmail.com>
2023-04-19 11:06:45 +0900

    [Design] 알림함 비어있음 UI 추가

--- src/screens/Alarm/index.js
+++ src/screens/Alarm/index.js
@@ -10,6 +10,9 @@ import { Text } from 'components/Text';
 import { Image } from 'components/Image';
 import NeedLogin from 'components/NeedLogin';
 
+//Assets
+import letterIcon from 'assets/icons/mypage-letter.png';
+
 export default function AlarmScreen({ navigation }) {
 
   const { state: { accountData } } = useContext(ApiContext);
@@ -18,19 +21,27 @@ export default function AlarmScreen({ navigation }) {
     <SafeArea>
       {
         accountData.loginStatus
-          ? (<>
-            <Container backgroundColor={COLOR.GRAY6} paddingHorizontal={20} paddingTop={30}>
-              <Text T3 bold>알림 기록</Text>
-            </Container>
-          </>)
-          
-          : (<>
-            <ContainerCenter backgroundColor={COLOR.GRAY6} paddingHorizontal={20}>
-              <NeedLogin marginTop={-40} action={() => navigation.navigate('LoginStackNavigation')}/>
-            </ContainerCenter>
-          </>)
+          ? <ContainerCenter backgroundColor={COLOR.GRAY6} paddingHorizontal={20}>
+            <AlarmEmptyContainer marginTop={-40}>
+              <Image source={letterIcon} width={70} height={74} />
+              <Text T3 bold marginTop={24}>알림함이 비어있습니다</Text>
+              <Text T6 medium center color={COLOR.GRAY1} marginTop={12}>비대면 진료와 관련된 안내 사항을{'\n'} 알림으로 보내드립니다</Text>
+            </AlarmEmptyContainer>
+          </ContainerCenter>
+
+          : <ContainerCenter backgroundColor={COLOR.GRAY6} paddingHorizontal={20}>
+            <NeedLogin marginTop={-40} action={() => navigation.navigate('LoginStackNavigation')} />
+          </ContainerCenter>
       }
 
     </SafeArea>
   );
 }
+
+const AlarmEmptyContainer = styled.View`
+  width: 100%;
+  padding: 40px 20px;
+  border-radius: 10px;
+  background-color: #FFFFFF;
+  align-items: center;
+`;
