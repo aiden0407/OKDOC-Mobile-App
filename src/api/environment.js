6788a218543ebbc346babcbadfbe140f383cfd5a
aiden0407 <cailyent0407@gmail.com>
2023-04-24 14:35:02 +0900

    [Add] 환경변수 파일 추가

---
+++ src/api/environment.js
@@ -0,0 +1,29 @@
+//* environment.js
+import Constants from "expo-constants";
+
+const ENV = {
+    dev: {
+        apiUrl: 'https://58715333-07ae-429d-a6d0-cfdc916aba7d.mock.pstmn.io',
+    },
+    staging: {
+        apiUrl: "[your.staging.api.here]",
+    },
+    prod: {
+        apiUrl: "[your.production.api.here]",
+    }
+};
+
+const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+    // What is __DEV__ ?
+    // This variable is set to true when react-native is running in Dev mode.
+    // __DEV__ is true when run locally, but false when published.
+    if (__DEV__) {
+        return ENV.dev;
+    } else if (env === 'staging') {
+        return ENV.staging;
+    } else if (env === 'prod') {
+        return ENV.prod;
+    }
+};
+
+export default getEnvVars;
