4c91729623268af9e20f8729ac1f434439c62f05
aiden0407 <cailyent0407@gmail.com>
2023-05-11 13:50:05 +0900

    [Update] 진료 입장 전 정보 수정 api 추가

--- src/api/History/index.js
+++ src/api/History/index.js
@@ -20,3 +20,24 @@ export const getScheduleByPatientId = async function (loginToken, patientId) {
         throw error.response;
     }
 }
+
+export const modifyTreatmentAppointmentBeforeEnter = async function (loginToken, appointmentId, symptom) {
+
+    try {
+        let options = {
+            url: `${apiUrl}/treatment_appointments/${appointmentId}`,
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${loginToken}`
+            },
+            data: {
+                "explain_symptom": symptom,
+            },
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error.response;
+    }
+}
