7338bcd6d11f932d0398197315e1669aefff181f
aiden0407 <cailyent0407@gmail.com>
2023-05-12 15:44:26 +0900

    [Delete] 마이페이지 관련 api 삭제

--- src/api/Login/index.js
+++ src/api/Login/index.js
@@ -4,7 +4,7 @@ import getEnvVars from 'api/environment.js';
 const { apiUrl } = getEnvVars();
 
 export const familyLocalLogin = async function (id, password) {
-    
+
     try {
         let options = {
             url: `${apiUrl}/authentication/LOGIN?identity=family`,
@@ -17,24 +17,6 @@ export const familyLocalLogin = async function (id, password) {
         const response = await axios(options);
         return response;
 
-    } catch (error) {
-        throw error;
-    }
-}
-
-export const getPatientList = async function (token) {
-
-    try {
-        let options = {
-            url: `${apiUrl}/patients/`,
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }
-        const response = await axios(options);
-        return response;
-        
     } catch (error) {
         throw error;
     }
