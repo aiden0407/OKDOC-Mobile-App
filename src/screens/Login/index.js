373129604025b9441a187c78a31b0dda6bf97394
aiden0407 <cailyent0407@gmail.com>
2023-06-13 17:12:29 +0900

    [Update] 로그인 로더 추가

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -5,7 +5,7 @@ import styled from 'styled-components/native';
 
 //Components
 import { COLOR } from 'constants/design'
-import { Alert } from 'react-native';
+import { Alert, ActivityIndicator } from 'react-native';
 import { SafeArea, KeyboardAvoiding, ContainerCenter, Center } from 'components/Layout';
 import { Text } from 'components/Text';
 import { Image } from 'components/Image';
@@ -24,9 +24,13 @@ export default function LoginPage({ navigation }) {
   const { dispatch } = useContext(ApiContext);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
   const passwordRef = useRef();
 
   const handleLogin = async function (email, password) {
+
+    setLoading(true);
+
     try {
       //const familyLocalLoginResponse = await familyLocalLogin(email, password);
       const familyLocalLoginResponse = await familyLocalLogin('패밀리아이디', 'r7csY|T66y');
@@ -57,12 +61,15 @@ export default function LoginPage({ navigation }) {
           etcConsideration: mainProfile?.etcConsideration,
         });
         navigation.pop(2);
+        setLoading(false);
 
       } catch (error) {
+        setLoading(false);
         Alert.alert('회원 정보 불러오기 실패');
       }
-      
+
     } catch (error) {
+      setLoading(false);
       Alert.alert('로그인 실패');
     }
   }
@@ -76,61 +83,80 @@ export default function LoginPage({ navigation }) {
   }
 
   return (
-    <SafeArea>
-      <KeyboardAvoiding>
-        <ContainerCenter paddingHorizontal={20}>
+    <>
+      {
+        loading && (
+          <LoadingBackground>
+            <ActivityIndicator size="large" color="#5500CC" />
+          </LoadingBackground>
+        )
+      }
+      <SafeArea>
+        <KeyboardAvoiding>
+          <ContainerCenter paddingHorizontal={20}>
+            <Center>
+              <Image source={mainLogo} width={182} height={40} />
+              <Text T5 medium color={COLOR.MAIN} marginTop={20}>해외에서도 <Text T5 bold color={COLOR.MAIN}>대학병원 전문의</Text>를 만나보세요</Text>
+            </Center>
 
-          <Center>
-            <Image source={mainLogo} width={182} height={40} />
-            <Text T5 medium color={COLOR.MAIN} marginTop={20}>해외에서도 <Text T5 bold color={COLOR.MAIN}>대학병원 전문의</Text>를 만나보세요</Text>
-          </Center>
+            <BorderInput
+              marginTop={30}
+              value={email}
+              onChangeText={setEmail}
+              placeholder="이메일"
+              autoComplete="email"
+              keyboardType="email-address"
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                passwordRef.current.focus();
+              }}
+            />
+            <BorderInput
+              marginTop={8}
+              value={password}
+              onChangeText={setPassword}
+              placeholder="비밀번호"
+              secureTextEntry
+              autoComplete="current-password"
+              returnKeyType="done"
+              onSubmitEditing={() => {
+                handleLogin(email, password);
+              }}
+              ref={passwordRef}
+            />
 
-          <BorderInput
-            marginTop={30}
-            value={email}
-            onChangeText={setEmail}
-            placeholder="이메일"
-            autoComplete="email"
-            keyboardType="email-address"
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              passwordRef.current.focus();
-            }}
-          />
-          <BorderInput
-            marginTop={8}
-            value={password}
-            onChangeText={setPassword}
-            placeholder="비밀번호"
-            secureTextEntry
-            autoComplete="current-password"
-            returnKeyType="done"
-            onSubmitEditing={() => {
-              handleLogin(email, password);
-            }}
-            ref={passwordRef}
-          />
+            <SolidButton
+              marginTop={120}
+              text="로그인"
+              action={() => handleLogin(email, password)}
+            />
+            <OutlineButton
+              marginTop={8}
+              text="회원가입"
+              action={() => handleRegister()}
+            />
 
-          <SolidButton
-            marginTop={120}
-            text="로그인"
-            action={() => handleLogin(email, password)}
-          />
-          <OutlineButton
-            marginTop={8}
-            text="회원가입"
-            action={() => handleRegister()}
-          />
+            <FindEmailPasswordContainer onPress={() => handleFindEmailPassword()}>
+              <Text T6 medium color={COLOR.GRAY2}>이메일/비밀번호가 기억나지 않나요?</Text>
+            </FindEmailPasswordContainer>
 
-          <FindEmailPasswordContainer onPress={() => handleFindEmailPassword()}>
-            <Text T6 medium color={COLOR.GRAY2}>이메일/비밀번호가 기억나지 않나요?</Text>
-          </FindEmailPasswordContainer>
-
-        </ContainerCenter>
-      </KeyboardAvoiding>
-    </SafeArea>
+          </ContainerCenter>
+        </KeyboardAvoiding>
+      </SafeArea>
+    </>
   );
 }
+
+const LoadingBackground = styled.Pressable`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #00000022;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const FindEmailPasswordContainer = styled.TouchableOpacity`
   margin-top: 40px;
