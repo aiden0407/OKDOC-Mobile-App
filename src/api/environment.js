ff6efe922aa8a53329d74d631fa2cbeed488bbf3
aiden0407 <cailyent0407@gmail.com>
2023-06-14 17:30:56 +0900

    [Update] 환경변수 api 토큰 추가

--- src/api/environment.js
+++ src/api/environment.js
@@ -4,6 +4,7 @@ import Constants from "expo-constants";
 const ENV = {
     dev: {
         apiUrl: 'https://58715333-07ae-429d-a6d0-cfdc916aba7d.mock.pstmn.io',
+        passportApiToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NzgxNzg4OTQsImV4cCI6MTY3ODY5NzI5NCwianRpIjoid2o0TExnNWd5MlNWIiwiYXBwbGljYXRpb25faWQiOiI4YTZiNmMzYi1mNDU3LTRmNmYtYjUyMC0zNzkwOTg4ZTZkZjYiLCJzdWIiOiIiLCJhY2wiOiIifQ.vJkzm05lSTv5tX3TawdEp5cf0K-BSXCuiMADz2F0pMi2f6MmgH-02gmpMguyqKDvJ5Icf2Tf1QMSCfYXhQLAgTV0jcMfYdpBA6XdWiWZrIb5Hhw1-C9jhnA3YluR28NWygFoHzzpzZQk2n_cakL9RoPF8HjtZWGO1NRtxoHlH4TemtC_VdGlaZ4NCXoUVKjayJG8R3GiqhoZfmJCvFSTB5mnUNhZIY_79gpNk4PvExy0KCtS_73_qWCB40-1i5KJ1DorW0Fbke8vT7MMwaAORrBWR2A9KJbSCackxpcFavhJcZalHih_VaYmjKefaqIgZlPrhbqEkQ0cwMkZzPKYGA',
     },
     staging: {
         apiUrl: "[your.staging.api.here]",
