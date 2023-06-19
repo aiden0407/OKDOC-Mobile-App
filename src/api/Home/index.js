476194bef617de253fff2b520b84da2146ae8ef1
aiden0407 <cailyent0407@gmail.com>
2023-06-19 14:45:44 +0900

    [Temp] 결제 목 데이터 변경

--- src/api/Home/index.js
+++ src/api/Home/index.js
@@ -114,35 +114,35 @@ export const getBiddings = async function (loginToken) {
     }
 }
 
-//export const postPaymentRequest = async function (loginToken) {
-export const postPaymentRequest = async function (orderNumber, productPrice, productName, patientName, patientPhone, patientEmail) {
+export const postPaymentRequest = async function () {
+//export const postPaymentRequest = async function (orderNumber, productPrice, productName, patientName, patientPhone, patientEmail) {
 
     try {
         let options = {
             url: `https://mobile.inicis.com/smart/payment/`,
             method: 'POST',
-            // data: {
-            //     P_INI_PAYMENT: 'CARD',
-            //     P_MID: 'insungif01',
-            //     P_OID: '123123123123123',
-            //     P_AMT: Number(5000),
-            //     P_GOODS: '[진료]홍길동의사-김길동환자, 2023년3월3일/수요일/오후3시30분 ~ 오후3시45분',
-            //     P_UNAME: '구매자패밀리명(환자명)',
-            //     P_NEXT_URL: 'https://m.ok-doc.com/',
-            //     P_MOBILE: '01012345678',
-            //     P_EMAIL: 'test@test.com',
-            // },
             data: {
                 P_INI_PAYMENT: 'CARD',
                 P_MID: 'insungif01',
-                P_OID: orderNumber,
-                P_AMT: Number(productPrice),
-                P_GOODS: productName,
-                P_UNAME: patientName,
+                P_OID: '123123123123123',
+                P_AMT: Number(5000),
+                P_GOODS: '[진료]홍길동의사-김길동환자, 2023년3월3일/수요일/오후3시30분 ~ 오후3시45분',
+                P_UNAME: '구매자패밀리명(환자명)',
                 P_NEXT_URL: 'https://m.ok-doc.com/',
-                P_MOBILE: patientPhone,
-                P_EMAIL: patientEmail,
+                P_MOBILE: '01012345678',
+                P_EMAIL: 'test@test.com',
             },
+            // data: {
+            //     P_INI_PAYMENT: 'CARD',
+            //     P_MID: 'insungif01',
+            //     P_OID: orderNumber,
+            //     P_AMT: Number(productPrice),
+            //     P_GOODS: productName,
+            //     P_UNAME: patientName,
+            //     P_NEXT_URL: 'https://m.ok-doc.com/',
+            //     P_MOBILE: patientPhone,
+            //     P_EMAIL: patientEmail,
+            // },
         }
         const response = await axios(options);
         return response;
