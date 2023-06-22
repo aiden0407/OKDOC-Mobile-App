e6b9b48153ba4e146a79d452c348fd2918e6a899
aiden0407 <cailyent0407@gmail.com>
2023-06-22 16:50:57 +0900

    [Temp] 문의하기 채널톡 localhost로 테스트

--- src/screens/MyPage/Inquiry/index.js
+++ src/screens/MyPage/Inquiry/index.js
@@ -1,15 +1,13 @@
-//React
-import styled from 'styled-components/native';
-
 //Components
-import { SafeArea, ContainerTop } from 'components/Layout';
+import { SafeArea } from 'components/Layout';
+import { WebView } from 'react-native-webview';
 
 export default function InquiryScreen() {
   return (
     <SafeArea>
-      <ContainerTop paddingTop={50}>
-
-      </ContainerTop>
+      <WebView
+        source={{ uri: 'http://localhost:5500/inquiry/?userName=%EC%97%90%EC%9D%B4%EB%93%A0&userEmail=aiden@insunginfo.co.kr' }}
+      />
     </SafeArea>
   );
 }
