f320db1f35264a56a675902891fec5ab45dd1f60
aiden0407 <cailyent0407@gmail.com>
2023-06-22 17:35:13 +0900

    [Update] 마이페이지 약관 Detail 수정

--- src/screens/MyPage/Policy/Detail/index.js
+++ src/screens/MyPage/Policy/Detail/index.js
@@ -1,29 +1,21 @@
-//React
-import { useEffect } from 'react';
-
 //Components
-import { COLOR } from 'constants/design'
-import { SafeArea, Container } from 'components/Layout';
-import { Text } from 'components/Text';
+import { SafeArea } from 'components/Layout';
+import { WebView } from 'react-native-webview';
 
-export default function PolicyDetialsScreen({ navigation, route }) {
+export default function PolicyDetailScreen({ route }) {
 
-  useEffect(() => {
-    navigation.setOptions({
-      title: route.params?.headerTitle
-    });
-  }, [navigation]);
-
-  const contentsText = `테스트용으로 쓰이는 더미 텍스트다. 디자인을 할 때 가제로 쓰이고 레이아웃을 테스트할 때 든지 프린트의 테스트, 기계 테스트, 폰트 테스트, 컨텐츠의 양을 측정할 때도 쓰인다. 테스트용으로 쓰이는 더미 텍스트다.\n\n테스트용으로 쓰이는 더미 텍스트다. 디자인을 할 때 가제로 쓰이고 레이아웃을 테스트할 때 든지 프린트의 테스트, 기계 테스트, 폰트 테스트, 컨텐츠의 양을 측정할 때도 쓰인다. 테스트용으로 쓰이는 더미 텍스트다.\n\n테스트용으로 쓰이는 더미 텍스트다. 디자인을 할 때 가제로 쓰이고 레이아웃을 테스트할 때 든지 프린트의 테스트, 기계 테스트, 폰트 테스트, 컨텐츠의 양을 측정할 때도 쓰인다. 테스트용으로 쓰이는 더미 텍스트다.`
+  const contentsText = route.params?.content;
 
   return (
     <SafeArea>
-      <Container paddingHorizontal={20}>
-
-        <Text T3 bold marginTop={30}>{route.params?.headerTitle}</Text>
-        <Text T6 marginTop={23} color={COLOR.GRAY1}>{contentsText}</Text>
-
-      </Container>
+      <WebView
+        source={{ html: contentsText }}
+        originWhitelist={['*']}
+        scalesPageToFit
+        onError={() => {
+          navigation.goBack();
+        }}
+      />
     </SafeArea>
   );
 }
