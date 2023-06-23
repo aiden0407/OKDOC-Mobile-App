6140fe2849c7b42c5dfc059fe206c651d956fd3f
aiden0407 <cailyent0407@gmail.com>
2023-06-23 10:46:49 +0900

    [Update] 문의하기 프로필 정보 연동

--- src/screens/MyPage/Inquiry/index.js
+++ src/screens/MyPage/Inquiry/index.js
@@ -1,12 +1,19 @@
+//React
+import { useContext } from 'react';
+import { ApiContext } from 'context/ApiContext';
+
 //Components
 import { SafeArea } from 'components/Layout';
 import { WebView } from 'react-native-webview';
 
 export default function InquiryScreen() {
+
+  const { state: ApiContextState } = useContext(ApiContext);
+
   return (
     <SafeArea>
       <WebView
-        source={{ uri: 'http://localhost:5500/inquiry/?userName=%EC%97%90%EC%9D%B4%EB%93%A0&userEmail=aiden@insunginfo.co.kr' }}
+        source={{ uri: `http://localhost:5500/inquiry/?userName=${ApiContextState.profileData[0].name}&userEmail=${ApiContextState.accountData.email}` }}
       />
     </SafeArea>
   );
