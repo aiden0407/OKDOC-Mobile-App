7f6f84a04032b4d102783da454e85b9bd03c182f
aiden0407 <cailyent0407@gmail.com>
2023-06-26 11:47:06 +0900

    [Fix] 날짜형식 Number -> String 으로 통일

--- src/screens/Login/Register/PassportInformation/index.js
+++ src/screens/Login/Register/PassportInformation/index.js
@@ -61,7 +61,7 @@ export default function PassportInformationScreen({ navigation }) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    return Number(year + month + day);
+    return year + month + day;
   }
 
   function formatDateString(dateString) {
