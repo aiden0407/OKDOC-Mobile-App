34b6f84575594e3ec1ffb2284d637651cb7b7caa
aiden0407 <cailyent0407@gmail.com>
2023-06-30 16:19:02 +0900

    [Update] 입찰 생성 로직 추가

--- src/screens/Home/Telemedicine/PaymentNotification/index.js
+++ src/screens/Home/Telemedicine/PaymentNotification/index.js
@@ -1,18 +1,23 @@
 //React
 import { useState, useContext } from 'react';
+import { ApiContext } from 'context/ApiContext';
 import { AppContext } from 'context/AppContext';
 import styled from 'styled-components/native';
 
 //Components
 import { COLOR } from 'constants/design';
-import { SYMPTOM, DEPARTMENT } from 'constants/service';
+import { Alert } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { SafeArea, ScrollView, Row, DividingLine, PaddingContainer } from 'components/Layout';
 import { Text } from 'components/Text';
 import { SolidButton } from 'components/Button';
 
+//Api
+import { createBidding } from 'api/Home';
+
 export default function PaymentNotificationScreen({ navigation }) {
 
+  const { state: { accountData } } = useContext(ApiContext);
   const { state: { telemedicineReservationStatus } } = useContext(AppContext);
   const [paymentAgreement, setPaymentAgreement] = useState(false);
   const [refundAgreement, setRefundAgreement] = useState(false);
@@ -29,8 +34,13 @@ export default function PaymentNotificationScreen({ navigation }) {
     // })
   }
 
-  function handleProceedPayment() {
-    navigation.navigate('TelemedicineReservationPayment', { screen: 'Payment' });
+  const handleProceedPayment = async function () {
+    try {
+      const response = await createBidding(accountData.loginToken, telemedicineReservationStatus);
+      navigation.navigate('TelemedicineReservationPayment', { screen: 'Payment' });
+    } catch (error) {
+      Alert.alert('예약 오류', '진료 예약 중 문제가 발생했습니다. 다시 시도해 주시기 바랍니다.');
+    }
   }
 
   return (
@@ -39,20 +49,20 @@ export default function PaymentNotificationScreen({ navigation }) {
         <PaddingContainer>
           <Text T3 bold marginTop={30}>진료를 위해{'\n'}예약하신 정보를 확인 해주세요</Text>
           <Row marginTop={24}>
-            <Text T3 bold color={COLOR.MAIN}>비용 120,000원</Text>
+            <Text T3 bold color={COLOR.MAIN}>비용 {telemedicineReservationStatus.product.price?.toLocaleString()}원</Text>
             <Text T3 bold color={COLOR.GRAY1}> (비급여 진료)</Text>
           </Row>
           <Row align marginTop={15}>
             <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6}/>
-            <Text T6 medium>{telemedicineReservationStatus.doctorInfo.name} 의사 (을지대학교병원)</Text>
+            <Text T6 medium>{telemedicineReservationStatus.doctorInfo.name} 의사 ({telemedicineReservationStatus.doctorInfo.hospital})</Text>
           </Row>
           <Row align marginTop={12}>
             <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6} />
-            <Text T6 medium>{telemedicineReservationStatus.doctorInfo.subject} / 진료</Text>
+            <Text T6 medium>{telemedicineReservationStatus.doctorInfo.subject} / {telemedicineReservationStatus.product.name}</Text>
           </Row>
           <Row align marginTop={12}>
             <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6}/>
-            <Text T6 medium>2023년 4월 {telemedicineReservationStatus.date} ({telemedicineReservationStatus.time})</Text>
+            <Text T6 medium>2023년 {telemedicineReservationStatus.date.substr(0, 2)}월 {telemedicineReservationStatus.date.substr(-2)}일 ({telemedicineReservationStatus.time})</Text>
           </Row>
           <Row align marginTop={12}>
             <Ionicons name="checkmark-sharp" size={18} color={COLOR.MAIN} marginRight={6}/>
