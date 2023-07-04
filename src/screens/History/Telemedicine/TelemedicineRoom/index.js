20bb77af2fad5d5af2209f54372f6c429c4d0bc5
aiden0407 <cailyent0407@gmail.com>
2023-07-04 17:09:58 +0900

    [Update] Zoom 웹뷰 부분 업데이트

--- src/screens/History/Telemedicine/TelemedicineRoom/index.js
+++ src/screens/History/Telemedicine/TelemedicineRoom/index.js
@@ -2,14 +2,27 @@
 import { SafeArea } from 'components/Layout';
 import { WebView } from 'react-native-webview';
 
-export default function TelemedicineRoomScreen() {
+export default function TelemedicineRoomScreen({ navigation, route }) {
 
   const telemedicineData = route.params.telemedicineData;
+
+  function handleTelemedicineComplete() {
+    navigation.replace('TelemedicineComplete', {
+      telemedicineData: telemedicineData,
+    });
+  }
 
   return (
     <SafeArea>
       <WebView
-        source={{ uri: `https://zoom.okdoc.app/patient/?meetingNumber=${telemedicineData.hospital_treatment_room.pmi}&userName=${telemedicineData.profileInfo.passport.user_name}` }}
+        source={{ uri: `https://zoom.okdoc.app/meeting/patient/?meetingNumber=${telemedicineData.hospital_treatment_room.pmi}&userName=${telemedicineData.profileInfo.passport.user_name}` }}
+        javaScriptEnabled={true}
+        allowsInlineMediaPlayback={true}
+        onNavigationStateChange={(navState) => {
+          if(navState.url.includes("https://zoom.okdoc.app/meeting/end")){
+            handleTelemedicineComplete();
+          }
+        }}
       />
     </SafeArea>
   );
