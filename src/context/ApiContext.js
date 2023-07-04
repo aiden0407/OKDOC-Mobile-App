112db497576a2370a87a8219abce3220a7c075ba
aiden0407 <cailyent0407@gmail.com>
2023-07-04 17:06:26 +0900

    [Refactor] historyData 예제 주석처리

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -147,61 +147,63 @@ const initialState = {
   //     ]]
   //   ]],
   // ]
-  historyData: {
-    underReservation: [
-      {
-        date: '2023.04.21',
-        time: '오전 11:30',
-        doctorInfo: {
-          name: '장윤희',
-          image: 'https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=',
-          hospital: '을지대병원',
-          subject: '이비인후과',
-          medicalField: ['5년차', '전문의'],
-        },
-        profileType: '본인',
-        profileInfo: {
-          name: '이준범'
-        },
-        symptom: '콧물이 계속 흘러요',
-      },
-    ],
-    pastHistory: [
-      {
-        date: '2023.04.07',
-        time: '오후 04:00',
-        doctorInfo: {
-          name: '김형도',
-          image: 'https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg',
-          hospital: '을지대병원',
-          subject: '내과',
-          medicalField: ['내과 수술', '20년차', '베테랑'],
-        },
-        profileType: '자녀',
-        profileInfo: {
-          name: '이재인'
-        },
-        symptom: '열이 좀 있는 것 같고 소화가 잘 안된대요',
-      },
-      {
-        date: '2023.04.06',
-        time: '오전 10:40',
-        doctorInfo: {
-          name: '해밍턴',
-          image: 'https://cdn.houstonpublicmedia.org/wp-content/uploads/2015/09/22105535/Nuila.Ricardo.E.143882.Internal.Med_.jpg',
-          hospital: '을지대병원',
-          subject: '신경외과',
-          medicalField: ['정신과 상담', '신경 수술', , '17년차', '베테랑'],
-        },
-        profileType: '본인',
-        profileInfo: {
-          name: '이준범'
-        },
-        symptom: '기침이 자주 나요',
-        opinion: 'pdf파일 링크',
-      },
-    ],
-  },
+  historyData: {},
+  // historyData 형식
+  //  {
+  //   underReservation: [
+  //     {
+  //       date: '2023.04.21',
+  //       time: '오전 11:30',
+  //       doctorInfo: {
+  //         name: '장윤희',
+  //         image: 'https://media.istockphoto.com/id/1189304032/photo/doctor-holding-digital-tablet-at-meeting-room.jpg?s=612x612&w=0&k=20&c=RtQn8w_vhzGYbflSa1B5ea9Ji70O8wHpSgGBSh0anUg=',
+  //         hospital: '을지대병원',
+  //         subject: '이비인후과',
+  //         medicalField: ['5년차', '전문의'],
+  //       },
+  //       profileType: '본인',
+  //       profileInfo: {
+  //         name: '이준범'
+  //       },
+  //       symptom: '콧물이 계속 흘러요',
+  //     },
+  //   ],
+  //   pastHistory: [
+  //     {
+  //       date: '2023.04.07',
+  //       time: '오후 04:00',
+  //       doctorInfo: {
+  //         name: '김형도',
+  //         image: 'https://www.pinnaclecare.com/wp-content/uploads/2017/12/bigstock-African-young-doctor-portrait-28825394.jpg',
+  //         hospital: '을지대병원',
+  //         subject: '내과',
+  //         medicalField: ['내과 수술', '20년차', '베테랑'],
+  //       },
+  //       profileType: '자녀',
+  //       profileInfo: {
+  //         name: '이재인'
+  //       },
+  //       symptom: '열이 좀 있는 것 같고 소화가 잘 안된대요',
+  //     },
+  //     {
+  //       date: '2023.04.06',
+  //       time: '오전 10:40',
+  //       doctorInfo: {
+  //         name: '해밍턴',
+  //         image: 'https://cdn.houstonpublicmedia.org/wp-content/uploads/2015/09/22105535/Nuila.Ricardo.E.143882.Internal.Med_.jpg',
+  //         hospital: '을지대병원',
+  //         subject: '신경외과',
+  //         medicalField: ['정신과 상담', '신경 수술', , '17년차', '베테랑'],
+  //       },
+  //       profileType: '본인',
+  //       profileInfo: {
+  //         name: '이준범'
+  //       },
+  //       symptom: '기침이 자주 나요',
+  //       opinion: 'pdf파일 링크',
+  //     },
+  //   ],
+  // },
   alarmData: [
 
   ],
