54b02c26b471a1980f9891f8d980e31746ab1c48
aiden0407 <cailyent0407@gmail.com>
2023-07-05 15:34:27 +0900

    [Update] 화상진료 StatusBarArea 색상 변경

--- src/screens/History/Telemedicine/TelemedicineRoom/index.js
+++ src/screens/History/Telemedicine/TelemedicineRoom/index.js
@@ -1,37 +1,49 @@
 //Components
-import { SafeArea } from 'components/Layout';
+import { StatusBarArea, SafeArea } from 'components/Layout';
+import { StatusBar } from 'expo-status-bar';
+import { useIsFocused } from '@react-navigation/native';
 import { WebView } from 'react-native-webview';
 
 export default function TelemedicineRoomScreen({ navigation, route }) {
 
   const telemedicineData = route.params.telemedicineData;
 
+  function FocusAwareStatusBar(props) {
+    const isFocused = useIsFocused();
+    return isFocused && <StatusBar {...props} />;
+  }
+
   function handleTelemedicineComplete() {
-    navigation.replace('TelemedicineComplete', {
+    navigation.navigate('TelemedicineWhetherFinished', {
       telemedicineData: telemedicineData,
     });
   }
 
   return (
-    <SafeArea>
-      <WebView
-        source={{ uri: `https://zoom.okdoc.app/meeting/patient/?meetingNumber=${telemedicineData.hospital_treatment_room.pmi}&userName=${telemedicineData.profileInfo.passport.user_name}` }}
-        //source={{ uri: `https://schmich.github.io/instascan/` }}
-        originWhitelist={['*']}
-        useWebkit
-        bounces
-        javaScriptEnabled
-        allowsInlineMediaPlayback
-        mediaPlaybackRequiresUserAction={false}
-        onNavigationStateChange={(navState) => {
-          if(navState.url.includes("https://zoom.okdoc.app/meeting/end")){
-            handleTelemedicineComplete();
-          }
-        }}
-        onError={(error) => {
-          throw error;
-        }}
-      />
-    </SafeArea>
+    <>
+      <StatusBarArea backgroundColor="#111111">
+        <FocusAwareStatusBar animated style="light" />
+      </StatusBarArea>
+      <SafeArea>
+        <WebView
+          source={{ uri: `https://zoom.okdoc.app/meeting/patient/?meetingNumber=${telemedicineData.hospital_treatment_room.pmi}&userName=${telemedicineData.profileInfo.passport.user_name}` }}
+          originWhitelist={['*']}
+          useWebkit
+          bounces
+          javaScriptEnabled
+          allowsInlineMediaPlayback
+          mediaPlaybackRequiresUserAction={false}
+          onNavigationStateChange={(navState) => {
+            if (navState.url.includes("https://zoom.okdoc.app/meeting/end")) {
+              handleTelemedicineComplete();
+            }
+          }}
+          onError={(error) => {
+            throw error;
+          }}
+        />
+      </SafeArea>
+      <StatusBarArea backgroundColor="#030303" />
+    </>
   );
 }
