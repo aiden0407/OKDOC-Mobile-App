e50432dd89386704938094f3db1f84e6bfdd2de0
aiden0407 <cailyent0407@gmail.com>
2023-07-05 17:28:06 +0900

    [Update] 남은 진료 시간 확인

--- src/screens/History/Telemedicine/TelemedicineWhetherFinished/index.js
+++ src/screens/History/Telemedicine/TelemedicineWhetherFinished/index.js
@@ -1,12 +1,56 @@
+//React
+import { useState, useEffect, useRef } from 'react';
+
 //Components
 import { COLOR } from 'constants/design'
-import { SafeArea, Container, ContainerCenter, Row } from 'components/Layout';
+import { Alert } from 'react-native';
+import { SafeArea, Container, ContainerCenter } from 'components/Layout';
 import { Text } from 'components/Text';
 import { SolidButton, OutlineButton } from 'components/Button';
 
 export default function TelemedicineWhetherFinishedScreen({ navigation, route }) {
 
   const telemedicineData = route.params.telemedicineData;
+  const [count, setCount] = useState(900);
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    const originalTime = new Date(telemedicineData.wish_at);
+    const addedTime = new Date(originalTime.getTime() + 15 * 60 * 1000);
+    const currentTime = new Date();
+    const remainingTime = Math.floor((addedTime - currentTime) / 1000);
+    setCount(remainingTime);
+
+    function tick() {
+      savedCallback.current();
+    }
+    const timer = setInterval(tick, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  const callback = () => {
+    if (count === 1) {
+      handleFinish();
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+  
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+  
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  }
 
   function handleNotFinish() {
     navigation.navigate('HistoryStackNavigation', { 
@@ -16,9 +60,13 @@ export default function TelemedicineWhetherFinishedScreen({ navigation, route })
   }
 
   function handleFinish() {
-    navigation.navigate('TelemedicineComplete', {
-      telemedicineData: telemedicineData,
-    });
+    if(count < 900){
+      navigation.navigate('TelemedicineComplete', {
+        telemedicineData: telemedicineData,
+      });
+    } else {
+      Alert.alert('진료 종료 확정 불가', '진료 입장 시간 전의 스케줄은 종료할 수 없습니다.');
+    }
   }
 
   return (
@@ -27,7 +75,8 @@ export default function TelemedicineWhetherFinishedScreen({ navigation, route })
         <ContainerCenter>
 
           <Text T2 bold marginTop={18}>진료가 끝났나요?</Text>
-          <Text T6 center color={COLOR.GRAY1} marginTop={18}>진료 종료를 확정해야만 화상 진료 통화방이 닫히고{'\n'}의사가 작성한 소견서를 확인할 수 있습니다.</Text>
+          <Text T4 bold color={COLOR.GRAY1} marginTop={6}>(자동 종료까지 남은 시간 {formatTime(count)})</Text>
+          <Text T6 center color={COLOR.GRAY1} marginTop={12}>진료 종료를 확정해야만 화상 진료 통화방이 닫히고{'\n'}의사가 작성한 소견서를 확인할 수 있습니다.</Text>
           <OutlineButton
             large
             marginTop={24}
