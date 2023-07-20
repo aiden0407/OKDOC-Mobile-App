8b7384893a762272daa889cab02994c4e5f7d844
aiden0407 <cailyent0407@gmail.com>
2023-07-20 13:40:31 +0900

    [Update] 인보이스 결제 완료창 업데이트

--- src/screens/History/Telemedicine/PaymentComplete/index.js
+++ src/screens/History/Telemedicine/PaymentComplete/index.js
@@ -1,23 +1,75 @@
 //React
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ApiContext } from 'context/ApiContext';
 import { AppContext } from 'context/AppContext';
-import styled from 'styled-components/native';
 
 //Components
 import { COLOR } from 'constants/design';
+import { Alert } from 'react-native';
 import { SafeArea, Container, Row, PaddingContainer } from 'components/Layout';
 import { Text } from 'components/Text';
 import { SolidButton } from 'components/Button';
 
-export default function PaymentConfirmScreen({ navigation, route }) {
+//Api
+import { getInvoiceInformation } from 'api/History';
+import { getPaymentInformation } from 'api/Home';
 
+export default function PaymentCompleteScreen({ navigation, route }) {
+
+  const { state: { accountData } } = useContext(ApiContext);
+  const { dispatch } = useContext(AppContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [invoiceData, setInvoiceData] = useState();
+  const [paymentData, setPaymentData] = useState();
+  const invoiceId = route.params?.invoiceId;
   const telemedicineData = route.params.telemedicineData;
 
+  useEffect(() => {
+    initInvoiceData()
+  }, []);
+
+  const initInvoiceData = async function () {
+    try {
+      const response = await getInvoiceInformation(accountData.loginToken, invoiceId);
+      setInvoiceData(response.data.response);
+      initPaymentData(response.data.response.P_TID);
+    } catch (error) {
+      Alert.alert('네트워크 오류로 인해 정보를 불러오지 못했습니다.');
+    }
+  }
+
+  const initPaymentData = async function (P_TID) {
+    try {
+      const response = await getPaymentInformation(P_TID);
+      setPaymentData(response.data.response);
+      setIsLoading(false);
+    } catch (error) {
+      Alert.alert('결제 정보를 불러오지 못했습니다.');
+    }
+  }
+
+  function formatDate(inputDate) {
+    const year = inputDate.slice(0, 4);
+    const month = inputDate.slice(4, 6);
+    const day = inputDate.slice(6, 8);
+    const hour = inputDate.slice(8, 10);
+    const minute = inputDate.slice(10, 12);
+    
+    const formattedDate = `${year}.${month}.${day} (${hour}:${minute})`;
+    return formattedDate;
+  }
+
   function handleConfirm() {
-    navigation.replace('HistoryStackNavigation', { 
-      screen: 'TelemedicineDetail' ,
+    dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: undefined});
+    dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: telemedicineData.id });
+    navigation.navigate('HistoryStackNavigation', { 
+      screen: 'TelemedicineDetail',
       params: { telemedicineData: telemedicineData }
     });
+  }
+
+  if (isLoading) {
+    return null;
   }
 
   return (
@@ -26,21 +78,21 @@ export default function PaymentConfirmScreen({ navigation, route }) {
         <Container>
         <PaddingContainer>
           <Text T3 bold marginTop={30}>결제가 완료되었어요</Text>
-          <Text T3 bold color={COLOR.MAIN} marginTop={9}>진료 연장 50,000원</Text>
+          <Text T3 bold color={COLOR.MAIN} marginTop={9}>비용 {Number(paymentData.price)?.toLocaleString()}원</Text>
           <Row marginTop={18}>
             <Text T6 medium color={COLOR.GRAY1} marginRight={42}>결제 금액</Text>
-            <Text T6 color={COLOR.GRAY1}>50,000원 | 일시불</Text>
+            <Text T6 color={COLOR.GRAY1}>{Number(paymentData.price)?.toLocaleString()}원 | 일시불</Text>
           </Row>
           <Row marginTop={6}>
             <Text T6 medium color={COLOR.GRAY1} marginRight={42}>결제 수단</Text>
-            <Text T6 color={COLOR.GRAY1}>삼성 마스타 5188</Text>
+            <Text T6 color={COLOR.GRAY1}>신용카드</Text>
           </Row>
           <Row marginTop={6}>
             <Text T6 medium color={COLOR.GRAY1} marginRight={42}>결제 일시</Text>
-            <Text T6 color={COLOR.GRAY1}>23.04.07 (11:30)</Text>
+            <Text T6 color={COLOR.GRAY1}>{formatDate(invoiceData?.P_AUTH_DT)}</Text>
           </Row>
         </PaddingContainer>
-
+        
         </Container>
 
         <PaddingContainer>
