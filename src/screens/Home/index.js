ef4d4e12dc9fe9ff16f164ed742a24bd81724bc1
aiden0407 <cailyent0407@gmail.com>
2023-07-20 16:34:06 +0900

    [Update] 홈 배너 링크 변경

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -91,7 +91,7 @@ export default function HomeScreen({ navigation }) {
       <SafeArea backgroundColor={COLOR.MAIN}>
         <ContainerTop>
 
-          <BannerContainer activeOpacity={0.8} onPress={() => Linking.openURL("https://okdoc.app/")}>
+          <BannerContainer activeOpacity={0.8} onPress={() => Linking.openURL("https://insunginfo.notion.site/9c30691e1c5b4ccbbd8b3d573e222a02?pvs=4")}>
             <Image source={bannerImage1} width={300} height={100} marginLeft={20} marginBottom={10} />
           </BannerContainer>
 
