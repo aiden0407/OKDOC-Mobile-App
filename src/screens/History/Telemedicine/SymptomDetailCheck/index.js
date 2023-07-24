c2626d2c5f3398f411e8d8be84afa1cd7269a903
aiden0407 <cailyent0407@gmail.com>
2023-07-24 09:09:54 +0900

    [Update] 진료 입장 시 데이터 수정 막기

--- src/screens/History/Telemedicine/SymptomDetailCheck/index.js
+++ src/screens/History/Telemedicine/SymptomDetailCheck/index.js
@@ -28,13 +28,23 @@ export default function SymptomDetailCheckScreen({ navigation, route }) {
   const [symptom, setSymptom] = useState(telemedicineData.explain_symptom);
 
   const submitSymptomDetail = async function () {
-    try {
-      await modifyTreatmentAppointmentBeforeEnter(accountData.loginToken, telemedicineData.id, symptom);
-      dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: undefined });
-      dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: telemedicineData.id });
+    if(telemedicineData.explain_symptom===symptom){
       handleNotice1();
-    } catch (error) {
-      Alert.alert('네트워크 오류로 인해 증상을 업데이트하지 못했습니다.');
+    }else{
+      try {
+        await modifyTreatmentAppointmentBeforeEnter(accountData.loginToken, telemedicineData.id, symptom);
+        dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: undefined });
+        dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: telemedicineData.id });
+        telemedicineData.explain_symptom = symptom;
+        Alert.alert('안내', '증상을 성공적으로 업데이트 하였습니다.', [
+          {
+            text: '확인',
+            onPress: () =>  handleNotice1()
+          },
+        ]);
+      } catch (error) {
+        Alert.alert('네트워크 오류로 인해 증상을 업데이트하지 못했습니다.');
+      }
     }
   }
 
@@ -52,7 +62,7 @@ export default function SymptomDetailCheckScreen({ navigation, route }) {
   }
 
   function handleNotice2() {
-    Alert.alert('안내', '화상 진료를 받기 위해서는 카메라와 마이크 사용 권한을 허가해야 합니다. 또한 정확한 의료 진단을 위해 화상 진료 녹화를 허가해야 합니다다.', [
+    Alert.alert('안내', '화상 진료를 받기 위해서는 카메라와 마이크 사용 권한을 허가해야 합니다. 또한 정확한 의료 진단을 위해 화상 진료 녹화를 허가해야 합니다.', [
       {
         text: '확인',
         onPress: () => handleConfirm()
@@ -73,40 +83,54 @@ export default function SymptomDetailCheckScreen({ navigation, route }) {
         <Container paddingHorizontal={20}>
           <Container>
             <Text T3 bold marginTop={30}>아래 내용을 확인해 주세요</Text>
-            <Text T6 medium color={COLOR.GRAY1} marginTop={12}>아래 내용은 진료 예약 시점에 입력하신 내용입니다{'\n'}달라진 점이 있거나, 추가할 내용이 있으면 수정해주세요</Text>
+            {/* <Text T6 medium color={COLOR.GRAY1} marginTop={12}>아래 내용은 진료 예약 시점에 입력하신 내용입니다{'\n'}달라진 점이 있거나, 추가할 내용이 있으면 수정해주세요</Text> */}
+            <Text T6 medium color={COLOR.GRAY1} marginTop={12}>아래 내용은 진료 예약 시점에 입력하신 내용입니다.{'\n'}달라진 점이 있거나, 추가하실 내용이 있으면{'\n'}진료 시작 시 말해주시기 바랍니다.</Text>
             <Text T6 bold marginTop={30}>증상</Text>
             <BoxInput
+              editable={false}
               large
               marginTop={12}
               placeholder="진료받고자 하는 증상을 서술해 주세요."
               value={symptom}
               onChangeText={setSymptom}
             />
-            {/* <Text T6 bold marginTop={30}>파일선택</Text>
+            <Text T6 bold marginTop={30}>첨부 파일 확인</Text>
 
             <IconContainer>
-              <IconColumn>
-                <IconButton onPress={() => console.log(1)}>
-                  <Image source={addImageIcon} width={44} height={44} marginLeft={4} marginBottom={4} />
-                </IconButton>
-                <Text T7 medium color={COLOR.GRAY1}>첨부사진 (1)</Text>
-              </IconColumn>
-              <IconColumn>
-                <IconButton onPress={() => console.log(2)}>
-                  <Image source={addImageIcon} width={44} height={44} marginLeft={4} marginBottom={4} />
-                </IconButton>
-                <Text T7 medium color={COLOR.GRAY1}>첨부사진 (2)</Text>
-              </IconColumn>
+              {
+                telemedicineData?.attachments?.[0]
+                  ? <IconColumn>
+                    <IconButton onPress={() => { }}>
+                      <Image source={{ uri: telemedicineData.attachments?.[0].Location }} width={85} height={80} borderRadius={8} />
+                    </IconButton>
+                    <Text T7 medium color={COLOR.GRAY1}>첨부사진 (1)</Text>
+                  </IconColumn>
+                  : <IconColumn>
+                    <IconButton onPress={() => { }}>
+                      <Image source={addImageIcon} width={44} height={44} marginLeft={4} marginBottom={4} />
+                    </IconButton>
+                    <Text T7 medium color={COLOR.GRAY1}>첨부사진 없음</Text>
+                  </IconColumn>
+              }
+              {
+                telemedicineData?.attachments?.[1]
+                && <IconColumn>
+                  <IconButton onPress={() => { }}>
+                    <Image source={{ uri: telemedicineData.attachments?.[1].Location }} width={85} height={80} borderRadius={8} />
+                  </IconButton>
+                  <Text T7 medium color={COLOR.GRAY1}>첨부사진 (2)</Text>
+                </IconColumn>
+              }
             </IconContainer>
 
-            <Row marginTop={18} align>
+            {/* <Row marginTop={18} align>
               <Ionicons name="alert-circle-outline" size={14} color={COLOR.GRAY2} marginRight={2} />
               <Text T8 color={COLOR.GRAY0} marginBottom={1}>10MB 이내 이미지 파일(jpg, png) 2개까지 첨부가 가능합니다.</Text>
             </Row> */}
           </Container>
 
           <SolidButton
-            text="진료실 입장하기"
+            text={telemedicineData.explain_symptom===symptom?"진료실 입장하기":"증상 수정하기"}
             marginBottom={20}
             disabled={!symptom}
             action={() => submitSymptomDetail()}
