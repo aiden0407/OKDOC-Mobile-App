d821108d37114b3d365f7e205d7485c6e89dd916
aiden0407 <cailyent0407@gmail.com>
2023-07-25 18:24:31 +0900

    [Update] 진료실 입장 시간 설정

--- src/screens/History/Telemedicine/SymptomDetailCheck/index.js
+++ src/screens/History/Telemedicine/SymptomDetailCheck/index.js
@@ -1,6 +1,5 @@
 //React
-import { useState, useContext } from 'react';
-import { ApiContext } from 'context/ApiContext';
+import { useState, useEffect, useRef, useContext } from 'react';
 import { AppContext } from 'context/AppContext';
 import styled from 'styled-components/native';
 
@@ -22,31 +21,58 @@ import { modifyTreatmentAppointmentBeforeEnter } from 'api/History';
 
 export default function SymptomDetailCheckScreen({ navigation, route }) {
 
-  const { state: { accountData } } = useContext(ApiContext);
   const { dispatch } = useContext(AppContext);
   const telemedicineData = route.params.telemedicineData;
   const [symptom, setSymptom] = useState(telemedicineData.explain_symptom);
+  const [count, setCount] = useState(301);
+  const savedCallback = useRef();
 
-  const submitSymptomDetail = async function () {
-    if(telemedicineData.explain_symptom===symptom){
-      handleNotice1();
-    }else{
-      try {
-        await modifyTreatmentAppointmentBeforeEnter(accountData.loginToken, telemedicineData.id, symptom);
-        dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: undefined });
-        dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: telemedicineData.id });
-        telemedicineData.explain_symptom = symptom;
-        Alert.alert('안내', '증상을 성공적으로 업데이트 하였습니다.', [
-          {
-            text: '확인',
-            onPress: () =>  handleNotice1()
-          },
-        ]);
-      } catch (error) {
-        Alert.alert('네트워크 오류로 인해 증상을 업데이트하지 못했습니다.');
-      }
+  useEffect(() => {
+    const originalTime = new Date(telemedicineData.wish_at);
+    const currentTime = new Date();
+    const remainingTime = Math.floor((originalTime - currentTime) / 1000);
+
+    setCount(remainingTime);
+
+    function tick() {
+      savedCallback.current();
+    }
+    const timer = setInterval(tick, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  const callback = () => {
+    if (count === 1) {
+      handleFinish();
+    } else {
+      setCount(count - 1);
     }
   }
+
+  // const submitSymptomDetail = async function () {
+  //   if(telemedicineData.explain_symptom===symptom){
+  //     handleNotice1();
+  //   }else{
+  //     try {
+  //       await modifyTreatmentAppointmentBeforeEnter(accountData.loginToken, telemedicineData.id, symptom);
+  //       dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: undefined });
+  //       dispatch({ type: 'HISTORY_DATA_ID_ADD', historyDataId: telemedicineData.id });
+  //       telemedicineData.explain_symptom = symptom;
+  //       Alert.alert('안내', '증상을 성공적으로 업데이트 하였습니다.', [
+  //         {
+  //           text: '확인',
+  //           onPress: () =>  handleNotice1()
+  //         },
+  //       ]);
+  //     } catch (error) {
+  //       Alert.alert('네트워크 오류로 인해 증상을 업데이트하지 못했습니다.');
+  //     }
+  //   }
+  // }
 
   function handleNotice1() {
     Alert.alert('진료실에 입장하시겠습니까?', '진료는 예약하신 시간부터 시작됩니다.', [
@@ -82,9 +108,8 @@ export default function SymptomDetailCheckScreen({ navigation, route }) {
       <KeyboardAvoiding>
         <Container paddingHorizontal={20}>
           <Container>
-            <Text T3 bold marginTop={30}>아래 내용을 확인해 주세요</Text>
-            {/* <Text T6 medium color={COLOR.GRAY1} marginTop={12}>아래 내용은 진료 예약 시점에 입력하신 내용입니다{'\n'}달라진 점이 있거나, 추가할 내용이 있으면 수정해주세요</Text> */}
-            <Text T6 medium color={COLOR.GRAY1} marginTop={12}>아래 내용은 진료 예약 시점에 입력하신 내용입니다.{'\n'}달라진 점이 있거나, 추가하실 내용이 있으면{'\n'}진료 시작 시 말해주시기 바랍니다.</Text>
+            <Text T3 bold marginTop={30}>증상을 확인해 주세요</Text>
+            <Text T6 medium color={COLOR.GRAY1} marginTop={12}>아래 내용은 진료 예약 시점에 입력하신 내용입니다.{'\n'}변경된 증상이 있다면 진료 중 말씀해 주시기 바랍니다.{'\n'}진료실 입장 버튼은 예약시간 5분 전부터 활성화됩니다.</Text>
             <Text T6 bold marginTop={30}>증상</Text>
             <BoxInput
               editable={false}
@@ -128,12 +153,12 @@ export default function SymptomDetailCheckScreen({ navigation, route }) {
               <Text T8 color={COLOR.GRAY0} marginBottom={1}>10MB 이내 이미지 파일(jpg, png) 2개까지 첨부가 가능합니다.</Text>
             </Row> */}
           </Container>
-
+          
           <SolidButton
-            text={telemedicineData.explain_symptom===symptom?"진료실 입장하기":"증상 수정하기"}
+            text="진료실 입장하기"
             marginBottom={20}
-            disabled={!symptom}
-            action={() => submitSymptomDetail()}
+            disabled={count>300}
+            action={() => handleNotice1()}
           />
         </Container>
       </KeyboardAvoiding>
