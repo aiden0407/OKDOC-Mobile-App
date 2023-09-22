17316cb6b8dfbf64f2e6bd0ec7ade9ce1ba730fa
aiden0407 <cailyent0407@gmail.com>
2023-09-22 13:37:45 +0900

    [Update] PASS 인증 결과값으로 계정 및 환자 정보 생성

--- src/screens/Login/Register/PhoneInformation/index.js
+++ src/screens/Login/Register/PhoneInformation/index.js
@@ -1,9 +1,8 @@
 //React
-import { useState, useContext, useRef } from 'react';
+import { useContext } from 'react';
 import { ApiContext } from 'context/ApiContext';
 import { AppContext } from 'context/AppContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import styled from 'styled-components/native';
 import IMP from 'iamport-react-native';
 
 //Components
@@ -22,9 +21,52 @@ export default function EmailPasswordScreen({ navigation }) {
     company: '인성정보',
   };
 
-  function callback(response) {
+  const callback = async function (response) {
     if(response.success){
+      try {
+        const deviceType = await AsyncStorage.getItem('@device_type');
+        const deviceToken = await AsyncStorage.getItem('@device_token');
+        const createFamilyAccountResponse = await createFamilyAccount(registerStatus.email, registerStatus.password, registerStatus.policy, deviceType, deviceToken);
+        const loginToken = createFamilyAccountResponse.data.response.accessToken;
+        initPatient(loginToken, response.imp_uid);
+      } catch (error) {
+        Alert.alert('계정 생성에 실패하였습니다. 다시 시도해 주시기 바랍니다.');
+      }
+    }
+  }
 
+  const initPatient = async function (loginToken, imp_uid) {
+    try {
+      const createPatientByPassAppResponse = await createPatientByPassApp(loginToken, imp_uid);
+      const mainProfile = createPatientByPassAppResponse.data.response;
+      apiContextDispatch({ 
+        type: 'LOGIN', 
+        loginToken: loginToken,
+        email: registerStatus.email, 
+      });
+
+      try {
+        const accountData = {
+          loginToken: loginToken,
+          email: registerStatus.email,
+        };
+        await AsyncStorage.setItem('accountData', JSON.stringify(accountData));
+      } catch (error) {
+        console.log(error);
+      }
+
+      apiContextDispatch({
+        type: 'PROFILE_CREATE_MAIN',
+        id: mainProfile.id,
+        name: mainProfile.passapp_certification.name,
+        relationship: mainProfile.relationship,
+        birth: mainProfile.passapp_certification.birthday.replaceAll('-', ''),
+        gender: mainProfile.passapp_certification.birthday.gender.toUpperCase(),
+      });
+      appContextDispatch({type: 'REGISTER_COMPLETE'});
+      navigation.navigate('RegisterComplete');
+    } catch (error) {
+      Alert.alert('프로필 정보 생성에 실패하였습니다. 다시 시도해 주시기 바랍니다.');
     }
   }
 
