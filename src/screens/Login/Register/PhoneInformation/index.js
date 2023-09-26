db5e6b3338f5c8ad088e838d25fccb0a309ac70c
aiden0407 <cailyent0407@gmail.com>
2023-09-26 14:25:27 +0900

    [Fix] PASS 앱 회원가입 에러 수정

--- src/screens/Login/Register/PhoneInformation/index.js
+++ src/screens/Login/Register/PhoneInformation/index.js
@@ -7,6 +7,7 @@ import IMP from 'iamport-react-native';
 
 //Components
 import { SafeArea } from 'components/Layout';
+import { Alert } from 'react-native';
 
 //Api
 import { createFamilyAccount, createPatientByPassApp } from 'api/Login';
@@ -26,8 +27,10 @@ export default function EmailPasswordScreen({ navigation }) {
       try {
         const deviceType = await AsyncStorage.getItem('@device_type');
         const deviceToken = await AsyncStorage.getItem('@device_token');
+        //const deviceToken = '961ae45edebaf891a146995cad67d1390d47b63b1867c42c93b6c405911ae241'
         const createFamilyAccountResponse = await createFamilyAccount(registerStatus.email, registerStatus.password, registerStatus.policy, deviceType, deviceToken);
         const loginToken = createFamilyAccountResponse.data.response.accessToken;
+        //const loginToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikluc3VuZ2luZm9fdXNlcl9jcmVkZW50aWFsIn0.eyJlbWFpbCI6ImFpZGVuQGluc3VuZ2luZm8uY28ua3IiLCJyb2xlIjoiZmFtaWx5IiwiaWF0IjoxNjk1Njk0ODU4LCJleHAiOjE2OTkyOTQ4NTgsImF1ZCI6ImxvY2FsaG9zdDozMDAwIiwiaXNzIjoibG9jYWxob3N0OjMwMDAiLCJzdWIiOiJhaWRlbkBpbnN1bmdpbmZvLmNvLmtyIiwianRpIjoiMTY5NTY5NDg1ODM0NSJ9.cfud3ygrsk8yl3l5Wd6vAQYzvqR8bnMSzfZLOkjPdYk'
         initPatient(loginToken, response.imp_uid);
       } catch (error) {
         Alert.alert('계정 생성에 실패하였습니다. 다시 시도해 주시기 바랍니다.');
@@ -61,7 +64,7 @@ export default function EmailPasswordScreen({ navigation }) {
         name: mainProfile.passapp_certification.name,
         relationship: mainProfile.relationship,
         birth: mainProfile.passapp_certification.birthday.replaceAll('-', ''),
-        gender: mainProfile.passapp_certification.birthday.gender.toUpperCase(),
+        gender: mainProfile.passapp_certification.gender.toUpperCase(),
       });
       appContextDispatch({type: 'REGISTER_COMPLETE'});
       navigation.navigate('RegisterComplete');
