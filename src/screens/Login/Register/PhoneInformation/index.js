8835db317880673499f1c97d6e0883be392ea6de
aiden0407 <cailyent0407@gmail.com>
2023-11-02 13:11:47 +0900

    [Update] PASS 중복가입 방지 로직 추가

--- src/screens/Login/Register/PhoneInformation/index.js
+++ src/screens/Login/Register/PhoneInformation/index.js
@@ -11,6 +11,7 @@ import { Alert } from 'react-native';
 
 //Api
 import { createAppleAccount, createGoogleAccount, createLocalAccount, createPatientByPassApp } from 'api/Login';
+import { deleteFamilyAccout } from 'api/MyPage';
 
 export default function EmailPasswordScreen({ navigation }) {
 
@@ -64,7 +65,7 @@ export default function EmailPasswordScreen({ navigation }) {
         };
         await AsyncStorage.setItem('accountData', JSON.stringify(accountData));
       } catch (error) {
-        console.log(error);
+        //console.log(error);
       }
 
       apiContextDispatch({
@@ -78,7 +79,42 @@ export default function EmailPasswordScreen({ navigation }) {
       appContextDispatch({type: 'REGISTER_COMPLETE'});
       navigation.navigate('RegisterComplete');
     } catch (error) {
-      Alert.alert('프로필 정보 생성에 실패하였습니다. 다시 시도해 주시기 바랍니다.');
+      if(error.response.data.statusCode === 409){
+        try {
+          await deleteFamilyAccout(loginToken, registerStatus.email);
+          apiContextDispatch({ type: 'LOGOUT' });
+          try {
+            await AsyncStorage.removeItem('accountData');
+          } catch (error) {
+            //console.log(error);
+          }
+          Alert.alert('계정 안내', '이미 가입된 회원입니다. 로그인 혹은 계정 찾기를 진행해주시기 바랍니다.', '', [
+            {
+              text: '확인',
+              onPress: () => {
+                appContextDispatch({type: 'REGISTER_COMPLETE'});
+                navigation.popToTop();
+              }
+            }
+          ]);
+        } catch (error) {
+          //회원 탈퇴 실패
+          Alert.alert('네트워크 에러', '프로필 생성에 실패했습니다. 관리자에게 문의해 주시기 바랍니다.', '', [
+            {
+              text: '확인',
+              onPress: () => navigation.goBack()
+            }
+          ]);
+        }
+      } else {
+        //중복 프로필 생성 외 프로필 생성 실패
+        Alert.alert('네트워크 에러', '프로필 생성에 실패했습니다. 관리자에게 문의해 주시기 바랍니다.', '', [
+          {
+            text: '확인',
+            onPress: () => navigation.goBack()
+          }
+        ]);
+      }
     }
   }
 
