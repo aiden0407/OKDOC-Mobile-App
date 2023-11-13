3c406efe178c36c275828f24e079fecd9967a45e
aiden0407 <cailyent0407@gmail.com>
2023-11-13 14:26:26 +0900

    [Update] 프로덕션 API 도메인 교체

--- src/constants/api/index.js
+++ src/constants/api/index.js
@@ -1,5 +1,5 @@
 // production
-// export const APIURL = 'https://api.okdoc.app'
+export const APIURL = 'https://api.okdoc.app'
 
 // staging server
-export const APIURL = 'https://68bkl4pc-3000.asse.devtunnels.ms'
+// export const APIURL = 'https://68bkl4pc-3000.asse.devtunnels.ms'
