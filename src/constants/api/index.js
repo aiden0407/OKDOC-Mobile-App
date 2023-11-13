e44e49eaa5163b13414224e1e536515adedceb42
aiden0407 <cailyent0407@gmail.com>
2023-11-13 14:22:24 +0900

    [Update] 스테이징 서버용 도메인 추가

--- src/constants/api/index.js
+++ src/constants/api/index.js
@@ -1,1 +1,5 @@
-export const APIURL = 'https://api.okdoc.app'
+// production
+// export const APIURL = 'https://api.okdoc.app'
+
+// staging server
+export const APIURL = 'https://68bkl4pc-3000.asse.devtunnels.ms'
