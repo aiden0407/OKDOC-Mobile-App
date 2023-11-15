6b7c72362ed019adb54a2b94b2b2b03f601e5401
aiden0407 <cailyent0407@gmail.com>
2023-11-15 14:30:19 +0900

    [Test] 테스터 계정 목록 추가

--- src/hook/useTestAccount.js
+++ src/hook/useTestAccount.js
@@ -1,12 +1,23 @@
 export default function useTestAccount(email) {
 
     const testAccountList = [
+        // 에이든 계정
         'aiden@insunginfo.co.kr',
-        'cailyent0407@gmail.com', 
+        'cailyent0407@gmail.com',
+        // 로건 계정
         'logan@insunginfo.co.kr', 
         'zloganway@gmail.com', 
         'rlagudeh123@naver.com', 
-        'dbckd22@gmail.com'
+        // 레오 계정
+        'reo.l@insunginfo.co.kr',
+        'dbckd22@gmail.com',
+        'dldbckd22@naver.com',
+        // 무브 계정
+        'move@insunginfo.co.kr',
+        'on21life@gmail.com',
+        'cras10@gmail.com',
+        'on21life@naver.com',
+        'cras10@naver.com'
     ];
 
     return testAccountList.includes(email);
