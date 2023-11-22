71c2cdd6ae55b0c16bf24eb7850fc0b830928ab1
aiden0407 <cailyent0407@gmail.com>
2023-11-22 16:46:47 +0900

    [Update] api constant에 서버 목록 추가

--- src/constants/api/index.js
+++ src/constants/api/index.js
@@ -1,5 +1,8 @@
-// production
+// production server
 export const APIURL = 'https://api.okdoc.app'
 
 // staging server
+// export const APIURL = 'https://staging.api.okdoc.app'
+
+// dev server
 // export const APIURL = 'https://68bkl4pc-3000.asse.devtunnels.ms'
