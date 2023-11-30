f67696f0e105814ef34beab624b2702d40fe44ed
aiden0407 <cailyent0407@gmail.com>
2023-11-30 12:18:13 +0900

    [Update] 알림 목록 요청 파라미터 수정

--- src/api/Alarm/index.js
+++ src/api/Alarm/index.js
@@ -5,7 +5,7 @@ import { APIURL } from 'constants/api';
 export const getAlarmHistory = async function (loginToken, patientId) {
     try {
         let options = {
-            url: `${APIURL}/notifications/?patient_id=${patientId}`,
+            url: `${APIURL}/notifications/?patient_id=${patientId}&sort_by=createdAt&sort_order=-1&limit=100`,
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${loginToken}`
