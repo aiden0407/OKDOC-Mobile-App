e216d489899224ef9e9a198bce4dbcf7341e83ec
aiden0407 <cailyent0407@gmail.com>
2023-12-01 09:40:44 +0900

    [Test] 테스트 계정에 인성정보 계정만 유지

--- src/hook/useTestAccount.js
+++ src/hook/useTestAccount.js
@@ -3,21 +3,12 @@ export default function useTestAccount(email) {
     const testAccountList = [
         // 에이든 계정
         'aiden@insunginfo.co.kr',
-        'cailyent0407@gmail.com',
         // 로건 계정
-        'logan@insunginfo.co.kr', 
-        'zloganway@gmail.com', 
-        'rlagudeh123@naver.com', 
+        'logan@insunginfo.co.kr',
         // 레오 계정
         'reo.l@insunginfo.co.kr',
-        'dbckd22@gmail.com',
-        'dldbckd22@naver.com',
         // 무브 계정
         'move@insunginfo.co.kr',
-        'on21life@gmail.com',
-        'cras10@gmail.com',
-        'on21life@naver.com',
-        'cras10@naver.com'
     ];
 
     return testAccountList.includes(email);
